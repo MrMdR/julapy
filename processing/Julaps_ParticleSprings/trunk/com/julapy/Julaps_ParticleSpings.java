b06e44e6735eaa1b44962f1bde13a29e378c202f
julapy <julapy@81ff1be0-9049-0410-8291-4d9ff92d881a>
2008-06-10 14:25:44 +0000

    now supports x number of particles. particles can be added at runtime.

--- processing/Julaps_ParticleSprings/trunk/com/julapy/Julaps_ParticleSpings.java
+++ processing/Julaps_ParticleSprings/trunk/com/julapy/Julaps_ParticleSpings.java
@@ -8,6 +8,8 @@ package com.julapy;
 //
 ////////////////////////////////////////////////////////////////////////////////
 
+import java.util.ArrayList;
+
 import javax.media.opengl.GL;
 import damkjer.ocd.Camera;
 import processing.opengl.PGraphicsOpenGL;
@@ -23,11 +25,10 @@ public class Julaps_ParticleSpings extends PApplet
 	Vec3D camTarget;
 	Vec3D camPosition;
 	Vec3D camNormal;
-
-	Particle[] particles;
-	Spring[] springs;
+	
+	ArrayList<Particle> particles;
+	ArrayList<Spring> springs;
 	int nparticles = 0;
-	int nsprings = 0;
 	double gravitational = 0;
 	double viscousdrag = 0.1;
 	int particleCloudSize = 500;
@@ -73,6 +74,7 @@ public class Julaps_ParticleSpings extends PApplet
 		cam.jump( camX, camY, 500 );
 		cam.aim( 0, 0, 0 );
 		cam.feed();
+		cam.up();
 
 		camPosition.set( cam.position()[0], cam.position()[1], cam.position()[2] );
 		camTarget.set( cam.target()[0], cam.target()[1], cam.target()[2] );
@@ -86,93 +88,101 @@ public class Julaps_ParticleSpings extends PApplet
 		   
 		pgl.beginGL();
 
-		int i,p1,p2;
-		for (i=0;i<nparticles;i++)
-			particles[i].render();
+		int i, p1, p2;
+		Particle pp1, pp2;
+		for ( i=0; i<particles.size(); i++ )
+			particles.get( i ).render();
 
-		for (i=0;i<nsprings;i++) 
-		{
-			p1 = springs[i].from;
-			p2 = springs[i].to;
+		for ( i=0; i<springs.size(); i++ ) {
+			p1 	= springs.get( i ).from;
+			p2 	= springs.get( i ).to;
+			pp1	= particles.get( p1 );
+			pp2 = particles.get( p2 );
 			gl.glBegin( GL.GL_LINES );
-			gl.glColor4f( 1, 1, 1, 1 );
-			gl.glVertex3f( particles[p1].loc.x, particles[p1].loc.y, particles[p1].loc.z );
-			gl.glVertex3f( particles[p2].loc.x, particles[p2].loc.y, particles[p2].loc.z );
+			gl.glColor4f( 1, 1, 1, 0.1f );
+			gl.glVertex3f( pp1.loc.x, pp1.loc.y, pp1.loc.z );
+			gl.glVertex3f( pp2.loc.x, pp2.loc.y, pp2.loc.z );
 			gl.glEnd();
 		}
 
 		pgl.endGL();
 	}
 	
+	public void mousePressed ()
+	{
+		addParticle( );
+	}
+	
 	public void initParticles ()
 	{
 		int i;
-		nparticles	= 5;
-		nsprings	= 9;
+		nparticles	= 10;
 
-		particles	= new Particle[nparticles];
-		springs		= new Spring[nsprings];
+		particles	= new ArrayList<Particle>();
+		springs		= new ArrayList<Spring>();
 
-		/* Random positions */
-		for ( i=0; i<nparticles; i++ ) 
-		{
-			particles[i] 		= new Particle();
-			particles[i].mass 	= 1;
-			particles[i].fixed 	= false;
-			particles[i].loc.x	= random(particleCloudSize) - particleCloudSize/2;
-			particles[i].loc.y	= random(particleCloudSize) - particleCloudSize/2;
-			particles[i].loc.z	= random(particleCloudSize) - particleCloudSize/2;
+		/* Add particles */
+		for ( i=0; i<nparticles; i++ ) {
+			addParticle();
 		}
-
-		for ( i=0; i<nsprings; i++ ) 
-		{
-			springs[i]					= new Spring();
-			springs[i].springconstant	= 0.1f;
-			springs[i].dampingconstant	= 0.01f;
-			springs[i].restlength 		= 400;
-		}
-		
-		/* Edges */
-		springs[0].from  = 0; springs[0].to  = 1;
-		springs[1].from  = 0; springs[1].to  = 2;
-		springs[2].from  = 0; springs[2].to  = 3;
-		springs[3].from  = 4; springs[3].to  = 1;
-		springs[4].from  = 4; springs[4].to  = 2;
-		springs[5].from  = 4; springs[5].to  = 3;
-		springs[6].from  = 1; springs[6].to  = 2;
-		springs[7].from  = 2; springs[7].to  = 3;
-		springs[8].from  = 3; springs[8].to  = 1;
 	}
 
+	public void addParticle ( )
+	{
+		Particle p	= new Particle();
+		p.id		= particles.size();
+		p.mass 		= 1;
+		p.fixed 	= false;
+		p.loc.x		= random(particleCloudSize) - particleCloudSize/2;
+		p.loc.y		= random(particleCloudSize) - particleCloudSize/2;
+		p.loc.z		= random(particleCloudSize) - particleCloudSize/2;
+
+		int i;
+		for( i=0; i<particles.size(); i++ ) {
+			Particle pp = particles.get( i );
+			Spring s			= new Spring();
+			s.springconstant	= 0.1f;
+			s.dampingconstant	= 0.05f;
+			s.restlength 		= 300;
+			s.from				= p.id;
+			s.to				= pp.id;
+			springs.add( s );
+		}
+		
+		particles.add( p );
+	}
+	
 	public void updateParticles ( double dt, int method )
 	{
 		int i;
 		Particle[] ptmp;
-		Derivatives[] deriv = new Derivatives[nparticles];
+		Particle p;
+		Derivatives[] deriv = new Derivatives[particles.size()];
 
 		switch ( method )
 		{
 			case 0:                                   /* Euler */
 				calculateForces(  );
 				calculateDerivatives( deriv );
-				for ( i=0; i<nparticles; i++ ) 
+				for ( i=0; i<particles.size(); i++ ) 
 				{
-					particles[i].loc.x += deriv[i].dpdt.x * dt;
-					particles[i].loc.y += deriv[i].dpdt.y * dt;
-					particles[i].loc.z += deriv[i].dpdt.z * dt;
-					particles[i].vel.x += deriv[i].dvdt.x * dt;
-					particles[i].vel.y += deriv[i].dvdt.y * dt;
-					particles[i].vel.z += deriv[i].dvdt.z * dt;
+					p		= particles.get( i );
+					p.loc.x += deriv[i].dpdt.x * dt;
+					p.loc.y += deriv[i].dpdt.y * dt;
+					p.loc.z += deriv[i].dpdt.z * dt;
+					p.vel.x += deriv[i].dvdt.x * dt;
+					p.vel.y += deriv[i].dvdt.y * dt;
+					p.vel.z += deriv[i].dvdt.z * dt;
 				}
 			break;
 			
 			case 1:                                   /* Midpoint */
 				calculateForces( );
 				calculateDerivatives( deriv );
-				ptmp = new Particle[nparticles];
-				for ( i=0; i<nparticles; i++ ) 
+				ptmp = new Particle[particles.size()];
+				for ( i=0; i<particles.size(); i++ ) 
 				{
-					ptmp[i] = particles[i].copy();
+					ptmp[i] = particles.get( i ).copy();
 					ptmp[i].loc.x += deriv[i].dpdt.x * dt / 2;
 					ptmp[i].loc.y += deriv[i].dpdt.y * dt / 2;
 					ptmp[i].loc.z += deriv[i].dpdt.z * dt / 2;
@@ -182,14 +192,15 @@ public class Julaps_ParticleSpings extends PApplet
 				}
 				calculateForces( );
 				calculateDerivatives( deriv );
-				for ( i=0; i<nparticles; i++ ) 
+				for ( i=0; i<particles.size(); i++ ) 
 				{
-					particles[i].loc.x += deriv[i].dpdt.x * dt;
-					particles[i].loc.y += deriv[i].dpdt.y * dt;
-					particles[i].loc.z += deriv[i].dpdt.z * dt;
-					particles[i].vel.x += deriv[i].dvdt.x * dt;
-					particles[i].vel.y += deriv[i].dvdt.y * dt;
-					particles[i].vel.z += deriv[i].dvdt.z * dt;
+					p		= particles.get( i );
+					p.loc.x += deriv[i].dpdt.x * dt;
+					p.loc.y += deriv[i].dpdt.y * dt;
+					p.loc.z += deriv[i].dpdt.z * dt;
+					p.vel.x += deriv[i].dvdt.x * dt;
+					p.vel.y += deriv[i].dvdt.y * dt;
+					p.vel.z += deriv[i].dvdt.z * dt;
 				}
 			break;
 		}
@@ -197,57 +208,65 @@ public class Julaps_ParticleSpings extends PApplet
 
 	public void calculateForces ( )
 	{
-		int i, p1, p2;
+		int i = 0, p1 = 0, p2 = 0;
 		float len, dx, dy, dz;
 		Vec3D down	= new Vec3D( 0, 0, -1 );
 		Vec3D force = new Vec3D( );
+		Particle p, pp1, pp2;
+		Spring s;
 
-		for ( i=0; i<nparticles; i++ ) 
+		for ( i=0; i<particles.size(); i++ ) 
 		{
-			particles[i].force = new Vec3D();
-			if (particles[i].fixed)
+			p		= particles.get( i );
+			p.force = new Vec3D();
+			if ( p.fixed )
 				continue;
 
 			/* Gravitation */
-			particles[i].force.x += gravitational * particles[i].mass * down.x;
-			particles[i].force.y += gravitational * particles[i].mass * down.y;
-			particles[i].force.z += gravitational * particles[i].mass * down.z;
+			p.force.x += gravitational * p.mass * down.x;
+			p.force.y += gravitational * p.mass * down.y;
+			p.force.z += gravitational * p.mass * down.z;
 
 			/* Viscous drag */
-			particles[i].force.x -= viscousdrag * particles[i].vel.x;
-			particles[i].force.y -= viscousdrag * particles[i].vel.y;
-			particles[i].force.z -= viscousdrag * particles[i].vel.z;
+			p.force.x -= viscousdrag * p.vel.x;
+			p.force.y -= viscousdrag * p.vel.y;
+			p.force.z -= viscousdrag * p.vel.z;
 		}
 
 		/* Handle the spring interaction */
-		for ( i=0; i<nsprings; i++ ) 
+		for ( i=0; i<springs.size(); i++ ) 
 		{
-			p1 = springs[i].from;
-			p2 = springs[i].to;
-			dx = particles[p1].loc.x - particles[p2].loc.x;
-			dy = particles[p1].loc.y - particles[p2].loc.y;
-			dz = particles[p1].loc.z - particles[p2].loc.z;
+			s	= springs.get( i );
+			p1 = s.from;
+			p2 = s.to;
+
+			pp1 = particles.get( p1 );
+			pp2 = particles.get( p2 );
+			
+			dx = pp1.loc.x - pp2.loc.x;
+			dy = pp1.loc.y - pp2.loc.y;
+			dz = pp1.loc.z - pp2.loc.z;
 			len = sqrt( dx*dx + dy*dy + dz*dz );
-			force.x  = springs[i].springconstant  * (len - springs[i].restlength);
-			force.x += springs[i].dampingconstant * (particles[p1].vel.x - particles[p2].vel.x) * dx / len;
+			force.x  = s.springconstant  * (len - s.restlength);
+			force.x += s.dampingconstant * (pp1.vel.x - pp2.vel.x) * dx / len;
 			force.x *= - dx / len;
-			force.y  = springs[i].springconstant  * (len - springs[i].restlength);
-			force.y += springs[i].dampingconstant * (particles[p1].vel.y - particles[p2].vel.y) * dy / len;
+			force.y  = s.springconstant  * (len - s.restlength);
+			force.y += s.dampingconstant * (pp1.vel.y - pp2.vel.y) * dy / len;
 			force.y *= - dy / len;
-			force.z  = springs[i].springconstant  * (len - springs[i].restlength);
-			force.z += springs[i].dampingconstant * (particles[p1].vel.z - particles[p2].vel.z) * dz / len;
+			force.z  = s.springconstant  * (len - s.restlength);
+			force.z += s.dampingconstant * (pp1.vel.z - pp2.vel.z) * dz / len;
 			force.z *= - dz / len;
-			if (!particles[p1].fixed) 
+			if (!pp1.fixed) 
 			{
-				particles[p1].force.x += force.x;
-				particles[p1].force.y += force.y;
-				particles[p1].force.z += force.z;
+				pp1.force.x += force.x;
+				pp1.force.y += force.y;
+				pp1.force.z += force.z;
 			}
-			if (!particles[p2].fixed) 
+			if (!pp2.fixed) 
 			{
-				particles[p2].force.x -= force.x;
-				particles[p2].force.y -= force.y;
-				particles[p2].force.z -= force.z;
+				pp2.force.x -= force.x;
+				pp2.force.y -= force.y;
+				pp2.force.z -= force.z;
 			}
 		}
 	}
@@ -255,21 +274,24 @@ public class Julaps_ParticleSpings extends PApplet
 	public void calculateDerivatives ( Derivatives[] deriv )
 	{
 		int i;
+		Particle p;
 
-		for (i=0;i<nparticles;i++) 
+		for (i=0;i<particles.size();i++) 
 		{
+			p				= particles.get( i );
 			deriv[i]		= new Derivatives();
-			deriv[i].dpdt.x = particles[i].vel.x;
-			deriv[i].dpdt.y = particles[i].vel.y;
-			deriv[i].dpdt.z = particles[i].vel.z;
-			deriv[i].dvdt.x = particles[i].force.x / particles[i].mass;
-			deriv[i].dvdt.y = particles[i].force.y / particles[i].mass;
-			deriv[i].dvdt.z = particles[i].force.z / particles[i].mass;
+			deriv[i].dpdt.x = p.vel.x;
+			deriv[i].dpdt.y = p.vel.y;
+			deriv[i].dpdt.z = p.vel.z;
+			deriv[i].dvdt.x = p.force.x / p.mass;
+			deriv[i].dvdt.y = p.force.y / p.mass;
+			deriv[i].dvdt.z = p.force.z / p.mass;
 		}
 	}
 	
 	class Particle
 	{
+		int id;
 		float renderSize = 10;
 		float mass;
 		Vec3D loc;
@@ -326,6 +348,11 @@ public class Julaps_ParticleSpings extends PApplet
 			p.force 		= p.force.copy();
 			p.fixed 		= fixed;;
 			return p;
+		}
+		
+		public String toString()
+		{
+			return "particle " + id + " - loc :: " + loc.toString() + " - vel :: " + vel.toString();
 		}
 	}
 	
