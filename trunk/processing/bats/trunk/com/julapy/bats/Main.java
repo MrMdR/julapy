e3b6210615629af25cb16e1b002687bdb0632599
julapy <julapy@81ff1be0-9049-0410-8291-4d9ff92d881a>
2008-12-07 13:44:52 +0000

    making some progress in orientating the bat.

--- trunk/processing/bats/trunk/com/julapy/bats/Main.java
+++ trunk/processing/bats/trunk/com/julapy/bats/Main.java
@@ -1,31 +1,333 @@
 package com.julapy.bats;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
 import javax.media.opengl.GL;
+
+import com.julapy.camera.Camera;
+import com.julapy.math.TrigUtil;
+import com.julapy.opengl.Primitive;
+import com.julapy.opengl.TextureLoader;
 
 import processing.core.PApplet;
 import processing.opengl.PGraphicsOpenGL;
+import toxi.geom.Vec3D;
+import toxi.math.MathUtils;
 
 public class Main extends PApplet
 {
 	PGraphicsOpenGL pgl;
 	GL gl;
 	
+	TextureLoader texLoader;
+	int wingLCallList;
+	int wingRCallList;
+	Camera cam;
+	
+	Vec3D center = new Vec3D( 2000, 2000, 2000 );
+	
+	Bat bat;
+	
 	public void setup()
 	{
 		size( 1280, 720, OPENGL );
-		
-		frameRate( 25 );
-		
+		frameRate( 30 );
 		colorMode( RGB, 1.0f );
+		background( 0.7f );
 		
-		hint( ENABLE_OPENGL_4X_SMOOTH );
+		initGL();
+		initTextureList();
+		
+		cam = new Camera( this, center.x, center.y, center.z );
+		
+		bat = new Bat();
 	}
+	
+	//////////////////////////////////////////////
+	// INIT.
+	//////////////////////////////////////////////
+	
+	public void initGL ()
+	{
+		pgl = (PGraphicsOpenGL) g;
+		gl = pgl.gl;
+
+		/* opengl init */
+		gl.glShadeModel( GL.GL_SMOOTH );              					// Enable Smooth Shading
+		gl.glClearColor( 0, 0, 0, 0.5f );		    					// Black Background
+		gl.glClearDepth( 1 );	                      					// Depth Buffer Setup
+		gl.glDepthFunc( GL.GL_LEQUAL );									// The Type Of Depth Testing To Do
+		gl.glHint( GL.GL_PERSPECTIVE_CORRECTION_HINT, GL.GL_NICEST );	// Really Nice Perspective Calculations
+		gl.glEnable( GL.GL_TEXTURE_2D );
+		gl.glDisable( GL.GL_DEPTH_TEST );								// Enable Depth Testing
+		gl.glEnable( GL.GL_BLEND );
+		
+        /* load texture */
+		texLoader = new TextureLoader( gl, 1 );
+		texLoader.loadTexture( loadImage( "data/bat.png" ), true );
+	}
+	
+	private void initTextureList ()
+	{
+		wingLCallList = gl.glGenLists( 1 );
+		gl.glNewList( wingLCallList, GL.GL_COMPILE );
+			gl.glBegin( GL.GL_QUADS );
+			gl.glNormal3f( 0, 0, 1 );
+			gl.glTexCoord2f( 0.0f, 0.0f );	gl.glVertex3f( -1, -0.5f, 0 );
+			gl.glTexCoord2f( 0.0f, 1.0f );	gl.glVertex3f( -1,  0.5f, 0 );
+			gl.glTexCoord2f( 0.5f, 1.0f );	gl.glVertex3f(  0,  0.5f, 0 );
+			gl.glTexCoord2f( 0.5f, 0.0f );	gl.glVertex3f(  0, -0.5f, 0 );
+			gl.glEnd();
+		gl.glEndList();
+
+		wingRCallList = gl.glGenLists( 1 );
+		gl.glNewList( wingRCallList, GL.GL_COMPILE );
+			gl.glBegin( GL.GL_QUADS );
+			gl.glNormal3f( 0, 0, 1 );
+			gl.glTexCoord2f( 0.5f, 0.0f );	gl.glVertex3f( 0, -0.5f, 0 );
+			gl.glTexCoord2f( 0.5f, 1.0f );	gl.glVertex3f( 0,  0.5f, 0 );
+			gl.glTexCoord2f( 1.0f, 1.0f );	gl.glVertex3f( 1,  0.5f, 0 );
+			gl.glTexCoord2f( 1.0f, 0.0f );	gl.glVertex3f( 1, -0.5f, 0 );
+			gl.glEnd();
+		gl.glEndList();
+	}
+	
+	//////////////////////////////////////////////
+	// DRAW.
+	//////////////////////////////////////////////
 	
 	public void draw ()
 	{
+		// clear.
+		background( 0.6f );
 		
+//		updateCamera();
+		
+		// render.
+		pgl.beginGL();
+		
+		gl.glDisable( GL.GL_DEPTH_TEST );
+		gl.glEnable( GL.GL_TEXTURE_2D );
+		gl.glBindTexture( GL.GL_TEXTURE_2D, texLoader.getTexture( 0 ) );
+		
+		bat.render( );
+		bat.update( );
+		
+		pgl.endGL();
 	}
 	
+	private void updateCamera ()
+	{
+		float dx, dy, theta, phi, radius;
+		
+		dx		= ( mouseX / (float)width - 0.5f ) * 2;
+		dy		= ( mouseY / (float)height - 0.5f ) * 2;
+		
+		theta	= dx * PI * 0.5f;
+		phi		= dy * PI * 0.5f;
+		if( dy < 0 )
+			phi = PI * 2 - phi;
+		radius	= -1000;
+		
+		cam.orbitTo( theta, phi, radius );
+		cam.update( );
+	}
+	
+	//////////////////////////////////////////////
+	// BAT CLASS.
+	//////////////////////////////////////////////
+	
+	public class Bat
+	{
+		float count		= 0;
+		float countInc	= 0.05f;
+		float countDir	= 1;
+
+		Vec3D[] locs;
+		Vec3D loc;
+		Vec3D vel;
+		
+		float wingW = 400;
+		float wingH	= 200;
+		
+		float wingMaxAngle		= 40;
+		float wingOffsetAngle	= 30;
+		float wingLAngle		= 0;
+		float wingRAngle		= 0;
+		
+		float orbitAngle		= 0;
+		float orbitAngleInc		= 1;
+		float orbitRadius		= 500;
+		
+		public Bat()
+		{
+			loc = new Vec3D( );
+			vel = new Vec3D( );
+			
+			locs = new Vec3D[ 50 ];
+			for( int i=0; i<locs.length; i++ )
+				locs[i] = new Vec3D( loc );
+		}
+
+		public void render ()
+		{
+			Vec3D out, up, right;
+			
+			out		= locs[ 0 ].sub( locs[ 1 ] ).normalize();
+			up		= loc.copy().normalize();
+			right	= up.cross( out );
+			
+			float rotX, rotY, rotZ;
+			
+			rotX	= out.headingYZ() * TrigUtil.RADTODEG;
+			rotY	= out.headingXZ() * TrigUtil.RADTODEG - 90;
+			rotZ	= out.headingXY() * TrigUtil.RADTODEG;
+			
+			float[] m;
+//			m = new float[]
+//			            {
+//							right.x, up.x, out.x, 0,
+//							right.y, up.y, out.y, 0,
+//							right.z, up.z, out.z, 0,
+//							0,	     0,	   0,	  1
+//						};
+
+			m = TrigUtil.eulerToMatrix( 0, -rotY, -rotZ );
+			
+			FloatBuffer rotMatrix;
+			rotMatrix = ByteBuffer.allocateDirect(4 * 4 * 4).order(ByteOrder.nativeOrder()).asFloatBuffer();
+			rotMatrix.put( m );
+			rotMatrix.flip();
+
+			gl.glPushMatrix();
+			
+			gl.glColor4f( 0, 0, 0, 1 );
+			gl.glTranslatef( center.x, center.y, center.z );
+			gl.glTranslatef( loc.x, loc.y, loc.z );
+			gl.glMultMatrixf( rotMatrix );
+			
+			gl.glPushMatrix();
+			gl.glRotatef( 90, 1, 0, 0 );
+			gl.glRotatef( wingLAngle, 0, 1, 0 );
+			gl.glScalef( wingW, wingH, 0 );
+			gl.glCallList( wingLCallList );
+			gl.glPopMatrix();
+
+			gl.glPushMatrix();
+			gl.glRotatef( 90, 1, 0, 0 );
+			gl.glRotatef( wingRAngle, 0, 1, 0 );
+			gl.glScalef( wingW, wingH, 0 );
+			gl.glCallList( wingRCallList );
+			gl.glPopMatrix();
+			
+			gl.glPopMatrix();
+		}
+		
+		public void update ()
+		{
+			countBounce();
+			calcWingAngle();
+			
+//			wander();
+//			pullToPoint( new Vec3D( 0, 0, 0 ) );
+//			orbitHorz();
+			orbitVert();
+			
+			savePosition();
+		}
+		
+		private void countBounce ()
+		{
+			count += countInc * countDir;
+			
+			if( count < 0 || count > 1 )
+			{
+				if( countDir == 1 )
+				{
+					count = 2 - count;
+				}
+				
+				if( countDir == -1 )
+				{
+					count = -count;
+				}
+				
+				countDir *= -1;
+			}
+		}
+		
+		private void countLinear ()
+		{
+			count += countInc;
+			
+			if( count > 1 )
+			{
+				count = count - 1;
+			}
+		}
+		
+		private void calcWingAngle()
+		{
+			wingLAngle = cos( count * PI ) * wingMaxAngle + wingOffsetAngle;
+			wingRAngle = cos( count * PI ) * -wingMaxAngle - wingOffsetAngle;
+		}
+		
+		private void wander ()
+		{
+			float rndRange;
+			
+			rndRange = 10;
+			
+			vel.x += random( rndRange ) - rndRange * 0.5f;
+			vel.y += random( rndRange ) - rndRange * 0.5f;
+			vel.z += random( rndRange ) - rndRange * 0.5f;
+			
+			loc.addSelf( vel );
+		}
+		
+		private void pullToPoint ( Vec3D p )
+		{
+			Vec3D v;
+
+			v = p.sub( loc ).normalize();
+			v.scaleSelf( loc.distanceTo( new Vec3D() ) / 20 );
+			
+			loc.addSelf( v );
+		}
+		
+		private void orbitHorz ()
+		{
+			float p[];
+			
+			p = TrigUtil.getPointOnCircle( orbitAngle, 500 );
+			
+			loc.x = p[ 0 ];
+			loc.z = p[ 1 ];
+			
+			orbitAngle += orbitAngleInc;
+		}
+		
+		private void orbitVert ()
+		{
+			float p[];
+			
+			p = TrigUtil.getPointOnCircle( orbitAngle, 500 );
+			
+			loc.x = p[ 0 ];
+			loc.y = p[ 1 ];
+			
+			orbitAngle += orbitAngleInc;
+		}
+		
+		private void savePosition ()
+		{
+		    for( int i=locs.length-1; i>0; i-- )
+		    	locs[ i ].set( locs[ i-1 ] );
+		    locs[ 0 ].set( loc );
+		}
+	}
 	
 	static public void main( String args[] )
 	{
---
+++ trunk/processing/bats/trunk/com/julapy/math/TrigUtil.java
@@ -0,0 +1,97 @@
+package com.julapy.math;
+
+public class TrigUtil 
+{
+	public static final float PI			= 3.14159265358979323846f;
+	public static final float HALF_PI		= PI / 2;
+	public static final float THIRD_PI		= PI / 3;
+	public static final float QUARTER_PI	= PI / 4;
+	public static final float TWO_PI		= PI * 2.0f;
+	public static final float RADTODEG		= 180 / PI;		
+	public static final float DEGTORAD		= PI / 180;
+	
+	public static float[] getPointOnCircle( float angleDegrees, float circleRadius )
+	{
+		float x;
+		float y;
+		float p[];
+		
+		x = circleRadius * (float)Math.cos( angleDegrees * DEGTORAD );
+		y = circleRadius * (float)Math.sin( angleDegrees * DEGTORAD );
+
+		p		= new float[2];
+		p[ 0 ]	= x;
+		p[ 1 ]	= y;
+		
+		return p;
+	}
+	
+	public static float[] eulerToMatrix ( float degreesX, float degreesY, float degreesZ )
+	{
+		float angleX;
+		float angleY;
+		float angleZ;
+		
+		angleX	= degreesX * TrigUtil.DEGTORAD;
+		angleY	= degreesY * TrigUtil.DEGTORAD;
+		angleZ	= degreesZ * TrigUtil.DEGTORAD;
+		
+		float A, B, C, D, E, F, AD, BD;
+		float[] m;
+		
+		A	= (float)Math.cos( angleX );
+		B	= (float)Math.sin( angleX );
+	    C	= (float)Math.cos( angleY );
+	    D	= (float)Math.sin( angleY );
+	    E	= (float)Math.cos( angleZ );
+	    F	= (float)Math.sin( angleZ );
+	    AD	= A * D;
+	    BD	= B * D;
+	    
+	    m		= new float[16];
+	    m[0]	= C * E;
+	    m[1]	= -C * F;
+	    m[2]	= D;
+	    m[4] 	= BD * E + A * F;
+	    m[5]	= -BD * F + A * E;
+	    m[6]	= -B * C;
+	    m[8]	= -AD * E + B * F;
+	    m[9]	= AD * F + B * E;
+	    m[10]	=  A * C;
+	    m[3]	= m[7] = m[11] = m[12] = m[13] = m[14] = 0;
+	    m[15]	= 1;
+	    
+	    return m;
+	}
+	
+	public static float[] quaternionToMatrix ( float x, float y, float z, float w )
+	{
+		float xx, xy, xz, xw, yy, yz, yw, zz, zw;
+		float[] m;
+		
+	    xx	= x * x;
+	    xy	= x * y;
+	    xz	= x * z;
+	    xw	= x * w;
+	    yy	= y * y;
+	    yz	= y * z;
+	    yw	= y * w;
+	    zz	= z * z;
+	    zw	= z * w;
+	    
+	    m		= new float[ 16 ];
+	    m[0]	= 1 - 2 * ( yy + zz );
+	    m[1]	= 2 * ( xy - zw );
+	    m[2]	= 2 * ( xz + yw );
+	    m[4]	= 2 * ( xy + zw );
+	    m[5]	= 1 - 2 * ( xx + zz );
+	    m[6]	= 2 * ( yz - xw );
+	    m[8]	= 2 * ( xz - yw );
+	    m[9]	= 2 * ( yz + xw );
+	    m[10]	= 1 - 2 * ( xx + yy );
+	    m[3]	= m[7] = m[11] = m[12] = m[13] = m[14] = 0;
+	    m[15]	= 1;
+	    
+	    return m;
+	}
+}
