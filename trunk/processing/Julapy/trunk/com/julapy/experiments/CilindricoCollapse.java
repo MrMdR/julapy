4ea15497ed885adb9ab29d34bbcb2b5f77ed41c8
julapy <julapy@81ff1be0-9049-0410-8291-4d9ff92d881a>
2009-02-21 03:17:30 +0000

    trying to draw normals of the arc bar. only way to be sure that the normals are correct. and then can properly play with lighting.

--- trunk/processing/Julapy/trunk/com/julapy/experiments/CilindricoCollapse.java
+++ trunk/processing/Julapy/trunk/com/julapy/experiments/CilindricoCollapse.java
@@ -14,12 +14,18 @@ import com.julapy.utils.TileSaver;
 
 import processing.opengl.PGraphicsOpenGL;
 import processing.core.*;
+import toxi.geom.Vec3D;
 
 public class CilindricoCollapse extends PApplet 
 {
 	PGraphicsOpenGL pgl;
 	GL gl;
 
+	Boolean lightingEnabled	= true;
+	float[] lightAmbient	= { 0.5f, 1.0f, 0.2f, 1.0f };
+	float[] lightDiffuse	= { 1.0f, 1.0f, 1.0f, 1.0f };
+	float[] lightPosition	= { 0.0f, 0.0f, 2.0f, 1.0f };
+	
 	float sinLUT[];
 	float cosLUT[];	
 	float SINCOS_PRECISION=1.0f;
@@ -50,7 +56,12 @@ public class CilindricoCollapse extends PApplet
 	
 	public void keyPressed()
 	{
-		if(key == 'r')
+		if( key == 'l' )
+		{
+			lightingEnabled = !lightingEnabled;
+		}
+		
+		if( key == 'r' )
 		{
 			isRecording = !isRecording;
 			
@@ -58,7 +69,7 @@ public class CilindricoCollapse extends PApplet
 			if(!isRecording) println("stopped recording.");
 		}
 		
-		if( key=='t' )
+		if( key == 't' )
 		{
 			isTiling = !isTiling;
 			
@@ -86,6 +97,21 @@ public class CilindricoCollapse extends PApplet
 		pgl	= (PGraphicsOpenGL) g;
 		gl	= pgl.gl;
 		gl.setSwapInterval( 1 );
+		
+        gl.glShadeModel(GL.GL_SMOOTH);              				// Enable Smooth Shading
+        gl.glClearColor(0.0f, 0.0f, 0.0f, 0.5f);					// Black Background
+        gl.glClearDepth(1.0f);										// Depth Buffer Setup
+        gl.glEnable(GL.GL_DEPTH_TEST);								// Enables Depth Testing
+        gl.glDepthFunc(GL.GL_LEQUAL);								// The Type Of Depth Testing To Do
+        gl.glHint(GL.GL_PERSPECTIVE_CORRECTION_HINT, GL.GL_NICEST);	// Really Nice Perspective Calculations
+        gl.glDisable(GL.GL_TEXTURE_2D);
+        
+        // Set up lighting
+        gl.glLightfv(GL.GL_LIGHT1, GL.GL_AMBIENT, this.lightAmbient, 0);
+        gl.glLightfv(GL.GL_LIGHT1, GL.GL_DIFFUSE, this.lightDiffuse, 0);
+        gl.glLightfv(GL.GL_LIGHT1, GL.GL_POSITION, this.lightPosition, 0);
+        gl.glEnable(GL.GL_LIGHT1);
+        gl.glEnable(GL.GL_LIGHTING);
 	}
 	
 	private void initTileSaver ()
@@ -95,7 +121,7 @@ public class CilindricoCollapse extends PApplet
 	
 	private void initArcBars ()
 	{
-		arcBar = new ArcBar( 100, 0, 0 );
+		arcBar = new ArcBar( 30, 0, 0 );
 	}
 	
 	////////////////////////////////////////////
@@ -121,7 +147,7 @@ public class CilindricoCollapse extends PApplet
 		tiler.pre();
 		
 		background(0);
-
+		
 		// START DRAW.
 		pgl.beginGL();
 
@@ -132,6 +158,15 @@ public class CilindricoCollapse extends PApplet
 			width/2,
 			-( height / 2.0f ) / PApplet.tan( PApplet.PI * 60 /360.0f )
 		);
+		
+		if ( lightingEnabled )
+        {
+        	gl.glEnable( GL.GL_LIGHTING );
+        }
+        else
+        {
+        	gl.glDisable( GL.GL_LIGHTING );
+        }
 		
 		drawArcBars();
 		
@@ -171,6 +206,11 @@ public class CilindricoCollapse extends PApplet
 		float width;
 		float height;
 		float scale;
+		
+		float normLength = 20;
+		
+		Boolean drawNormals = false;
+		Boolean drawQuads	= true;
 		
 		public ArcBar ()
 		{
@@ -212,7 +252,11 @@ public class CilindricoCollapse extends PApplet
 			float x1, y1, x2, y2;
 			float px1, py1, px2, py2;
 			float p, r, g, b, a;
+			Vec3D vtemp, ntemp, n1, n2;
+			Vec3D[] vtemps;
+			float nLength;
 			int ang;
+			int i;
 			
 			x1 = y1 = x2 = y2 = 0;
 			px1 = py1 = px2 = py2 = 0;
@@ -225,9 +269,7 @@ public class CilindricoCollapse extends PApplet
 			gl.glRotatef( ry, 0, 1, 0 );
 			gl.glRotatef( rz, 0, 0, 1 );
 			
-			gl.glBegin( GL.GL_QUADS );
-			
-			for( int i = 0; i<ang; i++ ) 
+			for( i = 0; i<ang; i++ ) 
 			{
 				p = 1.0f - (float)i / (float)( ang - 1 );
 				r = 1;	// red
@@ -241,51 +283,112 @@ public class CilindricoCollapse extends PApplet
 				x2 = cosLUT[i] * ( radius + width );
 				y2 = sinLUT[i] * ( radius + width );
 				
-				gl.glColor4f( r, g, b, a );
+				// normal pointing inwards.
+				int dir = 1;
+				if( ( ang > 270 && ang < 360 ) || ( ang < 90 ) )
+					dir = -1;
 				
-				if( i == 0  || i == ( ang - 1 ) )
-				{
-					// draw end faces.
-					gl.glVertex3f( x1, y1, 0 );
-					gl.glVertex3f( x2, y2, 0 );
-					gl.glVertex3f( x2, y2, height );
-					gl.glVertex3f( x1, y1, height );
-				}
+				n1 = new Vec3D
+				(
+					(float)( Math.cos( i * DEG_TO_RAD ) * dir ),
+					(float)( Math.sin( i * DEG_TO_RAD ) * dir ),
+					0
+				);
 				
-				if( i != 0 )
+				if( drawNormals )
 				{
-					// draw inner faces.
-					gl.glVertex3f( x1, y1, 0 );
-					gl.glVertex3f( px1, py1, 0 );
-					gl.glVertex3f( px1, py1, height );
-					gl.glVertex3f( x1, y1, height );
-
-					// draw outer faces.
-					gl.glVertex3f( x2, y2, 0 );
-					gl.glVertex3f( px2, py2, 0 );
-					gl.glVertex3f( px2, py2, height );
-					gl.glVertex3f( x2, y2, height );
-
-					// draw bottom faces.
-					gl.glVertex3f( x1, y1, 0 );
-					gl.glVertex3f( px1, py1, 0 );
-					gl.glVertex3f( px2, py2, 0 );
-					gl.glVertex3f( x2, y2, 0 );
-
-					// draw top faces.
-					gl.glVertex3f( x1, y1, height );
-					gl.glVertex3f( px1, py1, height );
-					gl.glVertex3f( px2, py2, height );
-					gl.glVertex3f( x2, y2, height );
+					gl.glBegin( GL.GL_LINES );
+					
+					if( i == 0  || i == ( ang - 1 ) )
+					{
+						// draw end faces.
+					}
+					
+					if( ( i != 0 ) && ( i % 10 == 0 ) )
+					{
+						// draw inner face normals.
+	
+						// draw outer face normals.
+	
+						// draw bottom face normals.
+						ntemp		= new Vec3D( 0, 0, 1 );
+						vtemps		= new Vec3D[ 4 ];
+						vtemps[ 0 ]	= new Vec3D( x1, y1, 0 );
+						vtemps[ 1 ]	= new Vec3D( px1, py1, 0 );
+						vtemps[ 2 ]	= new Vec3D( px2, py2, 0 );
+						vtemps[ 3 ]	= new Vec3D( x2, y2, 0 );
+						
+						for( i=0; i<vtemps.length; i++ )
+						{
+							vtemp = vtemps[ i ];
+							
+							gl.glVertex3f( vtemp.x, vtemp.y, vtemp.z );
+							gl.glVertex3f
+							(
+								vtemp.x + ntemp.x * normLength,
+								vtemp.y + ntemp.y * normLength, 
+								vtemp.z + ntemp.z * normLength 
+							);
+						}
+						
+						// draw top face normals.
+					}
+					
+					gl.glEnd();
 				}
 
-				px1 = x1;
-				py1 = y1;
-				px2 = x2;
-				py2 = y2;
+				if( drawQuads )
+				{
+					gl.glBegin( GL.GL_QUADS );				
+					
+					// gl.glColor4f( r, g, b, a );
+					
+					if( i == 0  || i == ( ang - 1 ) )
+					{
+						// draw end faces.
+						gl.glVertex3f( x1, y1, 0 );
+						gl.glVertex3f( x2, y2, 0 );
+						gl.glVertex3f( x2, y2, height );
+						gl.glVertex3f( x1, y1, height );
+					}
+					
+					if( i != 0 )
+					{
+						// draw inner faces.
+						gl.glVertex3f( x1, y1, 0 );
+						gl.glVertex3f( px1, py1, 0 );
+						gl.glVertex3f( px1, py1, height );
+						gl.glVertex3f( x1, y1, height );
+	
+						// draw outer faces.
+						gl.glVertex3f( x2, y2, 0 );
+						gl.glVertex3f( px2, py2, 0 );
+						gl.glVertex3f( px2, py2, height );
+						gl.glVertex3f( x2, y2, height );
+	
+						// draw bottom faces.
+						gl.glNormal3f( 0, 0, 1 );
+						gl.glVertex3f( x1, y1, 0 );
+						gl.glVertex3f( px1, py1, 0 );
+						gl.glVertex3f( px2, py2, 0 );
+						gl.glVertex3f( x2, y2, 0 );
+	
+						// draw top faces.
+						gl.glNormal3f( 0, 0, -1 );
+						gl.glVertex3f( x1, y1, height );
+						gl.glVertex3f( px1, py1, height );
+						gl.glVertex3f( px2, py2, height );
+						gl.glVertex3f( x2, y2, height );
+					}
+					
+					gl.glEnd();
+	
+					px1 = x1;
+					py1 = y1;
+					px2 = x2;
+					py2 = y2;
+				}
 			}
-			
-			gl.glEnd();
 			
 			gl.glPopMatrix();
 		}
