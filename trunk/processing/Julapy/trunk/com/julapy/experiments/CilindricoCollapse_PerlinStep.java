f8bd692c369b6674d6158c1aa7a029546e935f92
julapy <julapy@81ff1be0-9049-0410-8291-4d9ff92d881a>
2009-03-01 09:37:34 +0000

    working on generating a step like pattern using perlin noise. will use this to hack the shit out of my arc bars.

---
+++ trunk/processing/Julapy/trunk/com/julapy/experiments/CilindricoCollapse_PerlinStep.java
@@ -0,0 +1,96 @@
+package com.julapy.experiments;
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Project: 		Julapy
+// File: 			CilindricoCollapse_PerlinStep.java
+// Created by: 		lukaszkarluk, Mar 1, 2009
+//
+////////////////////////////////////////////////////////////////////////////////
+
+import javax.media.opengl.GL;
+import processing.opengl.PGraphicsOpenGL;
+import processing.core.*;
+
+public class CilindricoCollapse_PerlinStep extends PApplet 
+{
+	float[] perlinSteps = {};
+	float perlinVal 	= 0;
+	float perlinCount	= 0;
+	float perlinInc		= 0.5f;
+	float perlinCue		= 10;
+
+	public void setup() 
+	{
+		size( 720, 360 );
+		frameRate( 25 );
+		colorMode( RGB, 1.0f );
+
+		initPerlinSteps();
+	}
+
+	public void draw() 
+	{
+		background(0);
+
+		updatePerlinSteps();
+		drawPerlinSteps();
+	}
+	
+	private void initPerlinSteps()
+	{
+		int i;
+		
+		perlinSteps = new float[ 360 ];
+		for( i=perlinSteps.length-1; i>=0; i-- )
+		{
+			perlinVal			= noise( perlinCount += perlinInc );
+			perlinVal			= (int)( perlinVal * 10 ) / (float)10;
+		    perlinSteps[ i ]	= perlinVal;
+		}
+	}
+	
+	private void updatePerlinSteps ()
+	{
+		int i;
+		
+	    for( i=perlinSteps.length-1; i>0; i-- )
+	    {
+	    	perlinSteps[ i ] = perlinSteps[i-1];
+	    }
+
+		if( frameCount == perlinCue )
+		{
+		    perlinVal			= noise( perlinCount += perlinInc );
+		    perlinVal			= (int)( perlinVal * 10 ) / (float)10;
+		    perlinVal			= max( perlinVal, 0.0f );
+		    perlinVal			= min( perlinVal, 1.0f );
+		    perlinSteps[ 0 ]	= perlinVal;
+		    
+		    perlinCue			+= (int)random( 5, 30 );
+		}
+	    
+//	    println( perlinVal );
+	}
+	
+	private void drawPerlinSteps ()
+	{
+		float w;
+		int i;
+		
+		w = width / (float)perlinSteps.length;
+		
+		fill( 0.5f );
+		
+		for( i=0; i<perlinSteps.length; i++ )
+		{
+			rect
+			( 
+				i * w,
+				height * 0.5f, 
+				w,
+				-perlinSteps[ i ] * height * 0.5f
+			);
+		}
+	}
+}
