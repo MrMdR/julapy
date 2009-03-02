bb32453fe02f3827bce99480ae7a573e9fadaa50
julapy <julapy@81ff1be0-9049-0410-8291-4d9ff92d881a>
2009-03-02 09:48:26 +0000

    created a PerlinStep class.

--- processing/Julapy/trunk/com/julapy/experiments/CilindricoCollapse_PerlinStep.java
+++ processing/Julapy/trunk/com/julapy/experiments/CilindricoCollapse_PerlinStep.java
@@ -8,98 +8,140 @@ package com.julapy.experiments;
 //
 ////////////////////////////////////////////////////////////////////////////////
 
-import javax.media.opengl.GL;
-import processing.opengl.PGraphicsOpenGL;
 import processing.core.*;
+import toxi.math.MathUtils;
+import toxi.math.noise.PerlinNoise;
 
 public class CilindricoCollapse_PerlinStep extends PApplet 
 {
-	float[] perlinSteps = {};
-	float perlinVal 	= 0;
-	float perlinSeed	= 0;
-	float perlinInc		= 1.5f;
-	int perlinCount		= 0;
-	int perlinCue		= 10;
-
+	PerlinStep ps01;
+	PerlinStep ps02;
+	
 	public void setup() 
 	{
 		size( 720, 360 );
 		frameRate( 25 );
 		colorMode( RGB, 1.0f );
 
-		initPerlinSteps();
+		ps01 = new PerlinStep( 360, 1.5f, 5, 30 );
+		ps02 = new PerlinStep( 360, 1.5f, 1, 1 );
 	}
 
 	public void draw() 
 	{
 		background(0);
 
-		updatePerlinSteps();
+		ps01.update();
+		ps02.update();
+		
 		drawPerlinSteps();
 	}
 	
-	private void initPerlinSteps()
+	public void keyPressed()
 	{
-		int i;
-		
-		perlinSteps = new float[ 360 ];
-		for( i=perlinSteps.length-1; i>=0; i-- )
+		if( key == 'r' )
 		{
-			if( perlinCount++ == perlinCue )
-			{
-			    perlinVal			= noise( perlinSeed += perlinInc ) * 3 - 1.0f;
-			    perlinVal			= (int)( perlinVal * 10 ) / (float)10;
-			    perlinVal			= max( perlinVal, 0.0f );
-			    perlinVal			= min( perlinVal, 1.0f );
-
-			    perlinCue			+= (int)random( 5, 30 );
-			}
-			
-			perlinSteps[ i ] = perlinVal;
+			save("export" + frameCount + ".png");
 		}
 	}
-	
-	private void updatePerlinSteps ()
-	{
-		int i;
 		
-	    for( i=perlinSteps.length-1; i>0; i-- )
-	    {
-	    	perlinSteps[ i ] = perlinSteps[i-1];
-	    }
-
-		if( perlinCount++ == perlinCue )
-		{
-		    perlinVal			= noise( perlinSeed += perlinInc ) * 3 - 1.0f;
-		    perlinVal			= (int)( perlinVal * 10 ) / (float)10;
-		    perlinVal			= max( perlinVal, 0.0f );
-		    perlinVal			= min( perlinVal, 1.0f );
-		    perlinSteps[ 0 ]	= perlinVal;
-		    
-		    perlinCue			+= (int)random( 5, 30 );
-		}
-	    
-	    println( perlinVal );
-	}
-	
 	private void drawPerlinSteps ()
 	{
 		float w;
 		int i;
 		
-		w = width / (float)perlinSteps.length;
-		
 		fill( 0.5f );
+
+		// perlin step 01.
 		
-		for( i=0; i<perlinSteps.length; i++ )
+		w = width / (float)ps01.perlinSteps.length;
+		
+		for( i=0; i<ps01.perlinSteps.length; i++ )
 		{
 			rect
 			( 
 				i * w,
 				height * 0.5f, 
 				w,
-				-perlinSteps[ i ] * height * 0.5f
+				-ps01.perlinSteps[ i ] * height * 0.5f
 			);
+		}
+
+		// perlin step 02.
+		
+		w = width / (float)ps02.perlinSteps.length;
+		
+		for( i=0; i<ps02.perlinSteps.length; i++ )
+		{
+			rect
+			( 
+				i * w,
+				height,
+				w,
+				-ps02.perlinSteps[ i ] * height * 0.5f
+			);
+		}
+		
+	}
+	
+	public class PerlinStep
+	{
+		PerlinNoise	pn;
+		float[] perlinSteps	= {};
+		float perlinVal 	= 0;
+		float perlinSeed	= 0;
+		float perlinRes		= 1.5f;
+		int perlinCount		= 0;
+		int perlinCue		= 10;
+		int perlinCueMin	= 1;
+		int perlinCueMax	= 1;
+		
+		public PerlinStep( int stepsTotal, float perlinInc, int perlinCueMin, int perlinCueMax )
+		{
+			this.perlinRes		= perlinRes;
+			this.perlinCueMin	= perlinCueMin;
+			this.perlinCueMax	= perlinCueMax;	
+			
+			int i;
+			
+			pn = new PerlinNoise();
+			
+			perlinSteps = new float[ stepsTotal ];
+			for( i=perlinSteps.length-1; i>=0; i-- )
+			{
+				progressValue();
+				
+				perlinSteps[ i ] = perlinVal;
+			}
+		}
+		
+		private void progressValue ()
+		{
+			if( perlinCount++ == perlinCue )
+			{
+				perlinVal			= pn.noise( perlinSeed += perlinRes ) * 3 - 1.0f;
+			    perlinVal			= (int)( perlinVal * 10 ) / (float)10;
+			    perlinVal			= MathUtils.max( perlinVal, 0.0f );
+			    perlinVal			= MathUtils.min( perlinVal, 1.0f );
+
+			    perlinCue			+= (int)MathUtils.random( perlinCueMin, perlinCueMax );
+			}
+		}
+		
+		public void update ()
+		{
+			int i;
+			
+		    for( i=perlinSteps.length-1; i>0; i-- )
+		    {
+		    	perlinSteps[ i ] = perlinSteps[i-1];
+		    }
+
+		    progressValue();
+		    
+		    perlinSteps[ i ] = perlinVal;
+		    
+//		    println( perlinVal );
 		}
 	}
 }
