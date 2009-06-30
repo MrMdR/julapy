b23a8873d3f8411572959d6d24570d0b0ff93295
julapy <julapy@81ff1be0-9049-0410-8291-4d9ff92d881a>
2009-06-30 05:25:46 +0000

    done it! type now moves along the ribbons.

--- trunk/openframeworks/ribbonType/trunk/src/testApp.h
+++ trunk/openframeworks/ribbonType/trunk/src/testApp.h
@@ -4,7 +4,7 @@
 #include "ofMain.h"
 #include "ofxVectorMath.h"
 
-#define RIBBON_MAX_LENGTH  20
+#define RIBBON_MAX_LENGTH  100
 
 class testApp : public ofSimpleApp{
 
@@ -34,6 +34,7 @@ class testApp : public ofSimpleApp{
 
 		float rps[ RIBBON_MAX_LENGTH * 3 * 1 ];		// 3 points per ribbon location.
 		float rvt[ RIBBON_MAX_LENGTH * 3 * 2 ];		// 3 points per vertex and 2 vertex per ribbon section.
+		float rvd[ RIBBON_MAX_LENGTH * 3 * 1 ];		// ribbon vertex direction - 3 points per ribbon section - xyz.
 		float rcl[ RIBBON_MAX_LENGTH * 4 * 2 ];		// 4 colour values per vertex
 	
 		ofTrueTypeFont font;
