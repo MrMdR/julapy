fe60e65c00bad98a2a3f518b7fa5c72ee6de451e
julapy <julapy@81ff1be0-9049-0410-8291-4d9ff92d881a>
2009-07-01 15:02:37 +0000

    some good progress... shit loads of optimisation. with loads more to come.

--- openframeworks/ribbons/ribbonsLib/trunk/src/RibbonType.h
+++ openframeworks/ribbons/ribbonsLib/trunk/src/RibbonType.h
@@ -10,6 +10,15 @@
 #include "ofMain.h"
 #include "ofxVec3f.h"
 
+#define MAX_CHARACTERS_SUPPORTED	52
+
+class CharacterRect
+{
+	public:
+		float width;
+		float height;
+};
+
 class RibbonType
 {
 
@@ -20,20 +29,31 @@ public :
 	
 	void loadTrueTypeFont( string fontName, int size = 50 );
 	void drawTypeOnRibbon( string copy, float *ribbonPositionArray, float *ribbonDirectionArray, int ribbonSize );
+	void setKerning( float value );
 	
 private :
 	
+	void initCharacters();
+	int  getCharacterIndex( int c );
 	void drawLetter( int letter, float xOffset = 0, float yOffset = 0 );
 	void drawLetterVertices();
 	void clearLetterVertices();
 	
 	ofTrueTypeFont font;
 	int fontSize;
+	float kerning;
 	
 	float *ribbonPositions;
 	float *ribbonDirections;
+	float *ribbonLengths;
 	int ribbonLength;
 	
 	vector <float*> polyVertices;
 	int currentStartVertex;
+	
+	int		charactersTotal;
+	char	*characters;
+	vector <ofTTFCharacter> characterContours;
+	vector <CharacterRect> characterRectangles;
+	int		contourStartIndex;
 };
