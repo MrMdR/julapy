ea44c59b19cbbbffa28e0694bd9efebd62e66d47
julapy <julapy@81ff1be0-9049-0410-8291-4d9ff92d881a>
2009-07-25 08:43:57 +0000

    lots of different little additions. added FBO's for some of the op effects.

--- openframeworks/lightChasm/trunk/src/osc/OscReceiver.cpp
+++ openframeworks/lightChasm/trunk/src/osc/OscReceiver.cpp
@@ -64,20 +64,22 @@ void OscReceiver :: ping()
 		
 		for( int i=0; i<m.getNumArgs(); i++ )
 		{
+			int argType = m.getArgType( i );
+			
 			if( inputs.at( inputIndex ).valueType == OSC_RECEIVER_TYPE_INT && 
-			    m.getArgType( i ) == OFXOSC_TYPE_INT32 )
+			    argType == OFXOSC_TYPE_INT32 )
 			{
 				*( inputs[ inputIndex ].valueInt ) = m.getArgAsInt32( i );
 			}
 			
 			if( inputs.at( inputIndex ).valueType == OSC_RECEIVER_TYPE_FLOAT && 
-			    m.getArgType( i ) == OFXOSC_TYPE_FLOAT )
+			    argType == OFXOSC_TYPE_FLOAT )
 			{
 				*( inputs[ inputIndex ].valueFloat ) = m.getArgAsFloat( i );
 			}
 			
 			if( inputs.at( inputIndex ).valueType == OSC_RECEIVER_TYPE_BOOL && 
-			    m.getArgType( i ) == OFXOSC_TYPE_INT32 )
+			    argType == OFXOSC_TYPE_FLOAT )	// TouchOSC returns a float 0.0f and 1.0f instead of integar values.
 			{
 				if( m.getArgAsInt32( i ) == 0 )
 					*( inputs[ inputIndex ].valueBool ) = false;
