e98efd9b4f6a9377508bf37d101a86da6ca25d48
julapy <julapy@81ff1be0-9049-0410-8291-4d9ff92d881a>
2009-08-10 09:00:57 +0000

    opened up a lot more to control at run-time. fixed some bugs.

--- trunk/openframeworks/triangleField/trunk/src/testApp.cpp
+++ trunk/openframeworks/triangleField/trunk/src/testApp.cpp
@@ -15,6 +15,9 @@ void testApp :: setup()
 	glEnable( GL_DEPTH_TEST );
 	glDepthFunc( GL_LEQUAL );
 	
+	screenWidth  = ofGetWidth();
+	screenHeight = ofGetHeight();
+	
 	tileSaver.init( 10, 0, true );
 	
 	initColor();
@@ -26,6 +29,7 @@ void testApp :: initDebug()
 {
 	dbInc      = 1;
 	frameCount = 0;
+	showDebug  = true;
 }
 
 //////////////////////////////////////////////
@@ -42,9 +46,12 @@ void testApp :: initColor()
 		colors[ i ] = new float[ 4 ];
 	}
 	
-	addColor(   0,   0,   0, 255 );		// black.
+	addColor(   0,   0,   0, 255 );		// black alpha.
+	addColor(   0,   0,   0,   0 );		// black fill.
 	addColor(  25, 191,  71, 255 );		// becks green.
+	addColor(   7,  52,  16, 180 );		// dark green.
 	addColor( 210,   2, 169, 255 );		// fuschia.
+	addColor( 210,   2, 169,  50 );		// fuschia alpha.
 	addColor( 213, 213,   0, 255 );		// yellow.
 	addColor( 168, 255,   0, 255 );		// lime.
 	addColor( 255, 132,   9, 255 );		// orange.
@@ -72,46 +79,72 @@ void testApp :: initFields()
 	
 	for( int i=0; i<fieldsTotal; i++ )
 	{
-		fields[ i ].scale  = 1;
-		fields[ i ].cutoff = 1;
-		fields[ i ].noiseX = 0;
-		fields[ i ].noiseY = 0;
-		fields[ i ].noiseZ = 0;
-		fields[ i ].noiseXRes = 0.003;
-		fields[ i ].noiseYRes = 0.003;
-		fields[ i ].noiseZRes = 0.003;
-		fields[ i ].noiseXVel = 0;
-		fields[ i ].noiseYVel = 0;
-		fields[ i ].noiseZVel = 0;
+		fields[ i ].scale    = 1;
+		fields[ i ].scaleInc = 1;
+		fields[ i ].cutoff   = 1.0;
+		
+		fields[ i ].drawOutline = true;
+		
+		fields[ i ].noiseX = 0.0;
+		fields[ i ].noiseY = 0.0;
+		fields[ i ].noiseZ = 0.0;
+		
+		fields[ i ].noiseXInit = 0.0;
+		fields[ i ].noiseYInit = 0.0;
+		fields[ i ].noiseZInit = 0.0;
+		
+		fields[ i ].noiseXRes = 0.01;
+		fields[ i ].noiseYRes = 0.01;
+		fields[ i ].noiseZRes = 0.01;
+		
+		fields[ i ].noiseXVel = 0.0;
+		fields[ i ].noiseYVel = 0.0;
+		fields[ i ].noiseZVel = 0.005;
+		
+		fields[ i ].noiseXScl = screenHeight / (float)screenWidth;
+		fields[ i ].noiseYScl = 1.0;
 	}
 	
 	fields[ 0 ].sColor    = &colors[ 0 ][ 0 ];
-	fields[ 0 ].eColor    = &colors[ 1 ][ 0 ];
-	fields[ 0 ].scale     = 16;
-	fields[ 0 ].cutoff    = 1.0;
-	fields[ 0 ].noiseXVel = 0.01;
+	fields[ 0 ].eColor    = &colors[ 2 ][ 0 ];
+	fields[ 0 ].bsColor   = fields[ 0 ].sColor;
+	fields[ 0 ].beColor   = fields[ 0 ].eColor;
+	fields[ 0 ].scaleInc  = 4;
+	fields[ 0 ].scale     = pow( 2, fields[ 0 ].scaleInc );
+	fields[ 0 ].cutoff    = 0.0;
+	fields[ 0 ].noiseXVel = 0.0;
 	fields[ 0 ].noiseYVel = 0.01;
 
 	fields[ 1 ].sColor    = &colors[ 0 ][ 0 ];
-	fields[ 1 ].eColor    = &colors[ 2 ][ 0 ];
-	fields[ 1 ].scale     = 8;
-	fields[ 1 ].cutoff    = 0.4;
-	fields[ 1 ].noiseXVel = 0.02;
-	fields[ 1 ].noiseYVel = 0.02;
+	fields[ 1 ].eColor    = &colors[ 4 ][ 0 ];
+	fields[ 1 ].bsColor   = fields[ 1 ].sColor;
+	fields[ 1 ].beColor   = fields[ 1 ].eColor;
+	fields[ 1 ].scaleInc  = 3;
+	fields[ 1 ].scale     = pow( 2, fields[ 1 ].scaleInc );
+	fields[ 1 ].cutoff    = 0.6;
+	fields[ 1 ].noiseXVel = 0.0;
+	fields[ 1 ].noiseYVel = -0.01;
 
-	fields[ 2 ].sColor    = &colors[ 0 ][ 0 ];
-	fields[ 2 ].eColor    = &colors[ 1 ][ 0 ];
-	fields[ 2 ].scale     = 4;
-	fields[ 2 ].cutoff    = 0.5;
-	fields[ 2 ].noiseXVel = 0.03;
-	fields[ 2 ].noiseYVel = 0.03;
+	fields[ 2 ].sColor    = &colors[ 1 ][ 0 ];
+	fields[ 2 ].eColor    = &colors[ 2 ][ 0 ];
+	fields[ 2 ].bsColor   = fields[ 2 ].sColor;
+	fields[ 2 ].beColor   = fields[ 2 ].eColor;
+	fields[ 2 ].scaleInc  = 2;
+	fields[ 2 ].scale     = pow( 2, fields[ 2 ].scaleInc );
+	fields[ 2 ].cutoff    = 0.4;
+	fields[ 2 ].noiseXVel = 0.0;
+	fields[ 2 ].noiseYVel = 0.02;
 	
-	fields[ 3 ].sColor    = &colors[ 0 ][ 0 ];
-	fields[ 3 ].eColor    = &colors[ 2 ][ 0 ];
-	fields[ 3 ].scale     = 2;
-	fields[ 3 ].cutoff    = 0.3;
-	fields[ 3 ].noiseXVel = 0.04;
-	fields[ 3 ].noiseYVel = 0.04;
+	fields[ 3 ].sColor    = &colors[ 5 ][ 0 ];
+	fields[ 3 ].eColor    = &colors[ 4 ][ 0 ];
+	fields[ 3 ].bsColor   = fields[ 3 ].sColor;
+	fields[ 3 ].beColor   = fields[ 3 ].eColor;
+	fields[ 3 ].scaleInc  = 1;
+	fields[ 3 ].scale     = pow( 2, fields[ 3 ].scaleInc );
+	fields[ 3 ].cutoff    = 0.6;
+	fields[ 3 ].noiseXVel = 0.015;
+	fields[ 3 ].noiseYVel = 0.015;
+
 }
 
 //////////////////////////////////////////////
@@ -133,14 +166,18 @@ void testApp :: update()
 void testApp :: draw()
 {
 	ofBackground( 0, 0, 0 );
+	ofEnableAlphaBlending();
 	
-	glDepthMask( GL_FALSE );
-	glBlendFunc( GL_SRC_ALPHA, GL_ONE );
+//	glBlendFunc( GL_ONE, GL_ONE );
+//	glBlendFunc( GL_ONE, GL_ONE_MINUS_DST_ALPHA );
+//	glBlendFunc( GL_SRC_ALPHA, GL_ONE );
+//	glBlendFunc( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA );
 	
 	tileSaver.begin();
 	
-//	drawSquareNoise();
+//	drawSquareNoise( &fields[ 0 ] );
 //	drawTraingleStatic();
+//	drawTriangleNoise( &fields[ 0 ] );
 
 	for( int i=0; i<fieldsTotal; i++ )
 		drawTriangleNoise( &fields[ i ] );
@@ -152,42 +189,52 @@ void testApp :: draw()
 		screenGrabUtil.save();
 	}
 	
-	glDepthMask( GL_TRUE );
 	glBlendFunc( GL_ONE, GL_ZERO );
 
-	drawDebug();
+	if( showDebug )
+		drawDebug();
 }
 
 void testApp :: drawSquareNoise ( TriangleField *field )
 {
-	float size = 40.0;
+	float size = 10.0 * field->scale;
 	
-	int cy = ceil( ofGetHeight() / (float)size );
-	int cx = ceil( ofGetWidth() / (float)size );
+	int cy = ceil( screenHeight / size );
+	int cx = ceil( screenWidth / size );
 	
-	field->noiseX = frameCount * field->noiseXVel;
-	field->noiseY = frameCount * field->noiseYVel;
-	field->noiseZ = frameCount * field->noiseZVel;
+	field->noiseXInit = frameCount * field->noiseXVel;
+	field->noiseYInit = frameCount * field->noiseYVel;
+	field->noiseZInit = frameCount * field->noiseZVel;
 	
-	int c;
-	float n;
+	field->noiseZ = field->noiseZInit;
+	
+	int cr, cg, cb, ca;
+	float n, nx, ny;
 	
 	for( int y=0; y<cy; y++ )
 	{
-		field->noiseY += field->noiseYRes * size;
+		field->noiseY = field->noiseYInit + y * size * field->noiseYRes * field->noiseYScl;
 		
 		for( int x=0; x<cx; x++ )
 		{
-			field->noiseX += field->noiseXRes * size;
-			
+			field->noiseX = field->noiseXInit + x * size * field->noiseXRes * field->noiseXScl;
+
 			n = ( noise.noise( field->noiseX, field->noiseY, field->noiseZ ) + 1.0 ) * 0.5;
-			c = (int)( n * 255 );
 			
-			ofSetColor( c, c, c );
-			ofFill();
-			ofRect( x * size, y * size, size, size );
-			ofNoFill();
-			ofRect( x * size, y * size, size, size );
+			if( n < field->cutoff )
+			{
+				n  /= field->cutoff;
+				cr = (int)( n * ( field->eColor[ 0 ] - field->sColor[ 0 ] ) + field->sColor[ 0 ] );
+				cg = (int)( n * ( field->eColor[ 1 ] - field->sColor[ 1 ] ) + field->sColor[ 1 ] );
+				cb = (int)( n * ( field->eColor[ 2 ] - field->sColor[ 2 ] ) + field->sColor[ 2 ] );
+				ca = (int)( n * ( field->eColor[ 3 ] - field->sColor[ 3 ] ) + field->sColor[ 3 ] );
+				
+				ofSetColor( cr, cg, cb, ca );
+				ofFill();
+				ofRect( x * size, y * size, size, size );
+				ofNoFill();
+				ofRect( x * size, y * size, size, size );
+			}
 		}
 	}
 }
@@ -196,8 +243,8 @@ void testApp :: drawTraingleStatic ()
 {
 	float size = 40.0;
 	
-	int cy = ceil( ofGetHeight() / (float)size );
-	int cx = ceil( ofGetWidth() / (float)size );
+	int cy = ceil( screenHeight / size );
+	int cx = ceil( screenWidth / size );
 	
 	int c;
 	
@@ -251,23 +298,25 @@ void testApp :: drawTriangleNoise ( TriangleField *field )
 {
 	float size = 10.0 * field->scale;
 	
-	int cy = ceil( ofGetHeight() / (float)size );
-	int cx = ceil( ofGetWidth() / (float)size );
+	int cy = ceil( screenHeight / size );
+	int cx = ceil( screenWidth / size );
 	
-	field->noiseX = frameCount * field->noiseXVel;
-	field->noiseY = frameCount * field->noiseYVel;
-	field->noiseZ = frameCount * field->noiseZVel;
+	field->noiseXInit = frameCount * field->noiseXVel;
+	field->noiseYInit = frameCount * field->noiseYVel;
+	field->noiseZInit = frameCount * field->noiseZVel;
+	
+	field->noiseZ = field->noiseZInit + field->noiseZRes;
 	
 	int cr, cg, cb, ca;
 	float n, nx, ny;
 	
 	for( int y=0; y<cy; y++ )
 	{
-		field->noiseY += field->noiseYRes * size;
+		field->noiseY = field->noiseYInit + y * field->noiseYRes * field->noiseYScl * size;
 		
 		for( int x=0; x<cx; x++ )
 		{
-			field->noiseX += field->noiseXRes * size;
+			field->noiseX = field->noiseXInit + x * field->noiseXRes * field->noiseXScl * size;
 			
 			int orientation = 1;
 			if( x % 2 == 1 )
@@ -278,13 +327,14 @@ void testApp :: drawTriangleNoise ( TriangleField *field )
 			
 			if( orientation == 1 )
 			{
-				nx = field->noiseX +  0.25 * size * field->noiseXRes;
-				ny = field->noiseY +  0.75 * size * field->noiseYRes;
+				nx = field->noiseX +  0.25 * size * field->noiseXRes * field->noiseXScl;
+				ny = field->noiseY +  0.75 * size * field->noiseYRes * field->noiseYScl;
 				n  = ( noise.noise( nx, ny, field->noiseZ ) + 1.0 ) * 0.5;
 				
-				if( n < field->cutoff )
+				if( n > field->cutoff )
 				{
-					n  /= field->cutoff;
+					n  = ( n - field->cutoff ) / (float)( 1 - field->cutoff );
+					
 					cr = (int)( n * ( field->eColor[ 0 ] - field->sColor[ 0 ] ) + field->sColor[ 0 ] );
 					cg = (int)( n * ( field->eColor[ 1 ] - field->sColor[ 1 ] ) + field->sColor[ 1 ] );
 					cb = (int)( n * ( field->eColor[ 2 ] - field->sColor[ 2 ] ) + field->sColor[ 2 ] );
@@ -293,17 +343,28 @@ void testApp :: drawTriangleNoise ( TriangleField *field )
 					ofSetColor( cr, cg, cb, ca );
 					ofFill();
 					ofTriangle( x * size, y * size, x * size, ( y + 1 ) * size, ( x + 1 ) * size, ( y + 1 ) * size );
-					ofNoFill();
-					ofTriangle( x * size, y * size, x * size, ( y + 1 ) * size, ( x + 1 ) * size, ( y + 1 ) * size );
+					
+					if( fields->drawOutline )
+					{
+						cr = (int)( n * ( field->beColor[ 0 ] - field->bsColor[ 0 ] ) + field->bsColor[ 0 ] );
+						cg = (int)( n * ( field->beColor[ 1 ] - field->bsColor[ 1 ] ) + field->bsColor[ 1 ] );
+						cb = (int)( n * ( field->beColor[ 2 ] - field->bsColor[ 2 ] ) + field->bsColor[ 2 ] );
+						ca = (int)( n * ( field->beColor[ 3 ] - field->bsColor[ 3 ] ) + field->bsColor[ 3 ] );
+						
+						ofSetColor( cr, cg, cb, ca );
+						ofNoFill();
+						ofTriangle( x * size, y * size, x * size, ( y + 1 ) * size, ( x + 1 ) * size, ( y + 1 ) * size );
+					}
 				}
 				
-				nx = field->noiseX +  0.75 * size * field->noiseXRes;
-				ny = field->noiseY +  0.25 * size * field->noiseYRes;
+				nx = field->noiseX +  0.75 * size * field->noiseXRes * field->noiseXScl;
+				ny = field->noiseY +  0.25 * size * field->noiseYRes * field->noiseYScl;
 				n  = ( noise.noise( nx, ny, field->noiseZ ) + 1.0 ) * 0.5;
 
-				if( n < field->cutoff )
+				if( n > field->cutoff )
 				{
-					n  /= field->cutoff;
+					n  = ( n - field->cutoff ) / (float)( 1 - field->cutoff );
+					
 					cr = (int)( n * ( field->eColor[ 0 ] - field->sColor[ 0 ] ) + field->sColor[ 0 ] );
 					cg = (int)( n * ( field->eColor[ 1 ] - field->sColor[ 1 ] ) + field->sColor[ 1 ] );
 					cb = (int)( n * ( field->eColor[ 2 ] - field->sColor[ 2 ] ) + field->sColor[ 2 ] );
@@ -312,19 +373,30 @@ void testApp :: drawTriangleNoise ( TriangleField *field )
 					ofSetColor( cr, cg, cb, ca );
 					ofFill();
 					ofTriangle( x * size, y * size, ( x + 1 ) * size, y * size, ( x + 1 ) * size, ( y + 1 ) * size );
-					ofNoFill();
-					ofTriangle( x * size, y * size, ( x + 1 ) * size, y * size, ( x + 1 ) * size, ( y + 1 ) * size );
+					
+					if( fields->drawOutline )
+					{
+						cr = (int)( n * ( field->beColor[ 0 ] - field->bsColor[ 0 ] ) + field->bsColor[ 0 ] );
+						cg = (int)( n * ( field->beColor[ 1 ] - field->bsColor[ 1 ] ) + field->bsColor[ 1 ] );
+						cb = (int)( n * ( field->beColor[ 2 ] - field->bsColor[ 2 ] ) + field->bsColor[ 2 ] );
+						ca = (int)( n * ( field->beColor[ 3 ] - field->bsColor[ 3 ] ) + field->bsColor[ 3 ] );
+						
+						ofSetColor( cr, cg, cb, ca );
+						ofNoFill();
+						ofTriangle( x * size, y * size, ( x + 1 ) * size, y * size, ( x + 1 ) * size, ( y + 1 ) * size );
+					}
 				}
 			}
 			else
 			{
-				nx = field->noiseX +  0.25 * size * field->noiseXRes;
-				ny = field->noiseY +  0.25 * size * field->noiseYRes;
+				nx = field->noiseX +  0.25 * size * field->noiseXRes * field->noiseXScl;
+				ny = field->noiseY +  0.25 * size * field->noiseYRes * field->noiseYScl;
 				n  = ( noise.noise( nx, ny, field->noiseZ ) + 1.0 ) * 0.5;
 
-				if( n < field->cutoff )
+				if( n > field->cutoff )
 				{
-					n  /= field->cutoff;
+					n  = ( n - field->cutoff ) / (float)( 1 - field->cutoff );
+					
 					cr = (int)( n * ( field->eColor[ 0 ] - field->sColor[ 0 ] ) + field->sColor[ 0 ] );
 					cg = (int)( n * ( field->eColor[ 1 ] - field->sColor[ 1 ] ) + field->sColor[ 1 ] );
 					cb = (int)( n * ( field->eColor[ 2 ] - field->sColor[ 2 ] ) + field->sColor[ 2 ] );
@@ -333,17 +405,28 @@ void testApp :: drawTriangleNoise ( TriangleField *field )
 					ofSetColor( cr, cg, cb, ca );
 					ofFill();
 					ofTriangle( x * size, y * size, ( x + 1 ) * size, y * size, x * size, ( y + 1 ) * size );
-					ofNoFill();
-					ofTriangle( x * size, y * size, ( x + 1 ) * size, y * size, x * size, ( y + 1 ) * size );
+					
+					if( fields->drawOutline )
+					{
+						cr = (int)( n * ( field->beColor[ 0 ] - field->bsColor[ 0 ] ) + field->bsColor[ 0 ] );
+						cg = (int)( n * ( field->beColor[ 1 ] - field->bsColor[ 1 ] ) + field->bsColor[ 1 ] );
+						cb = (int)( n * ( field->beColor[ 2 ] - field->bsColor[ 2 ] ) + field->bsColor[ 2 ] );
+						ca = (int)( n * ( field->beColor[ 3 ] - field->bsColor[ 3 ] ) + field->bsColor[ 3 ] );
+						
+						ofSetColor( cr, cg, cb, ca );
+						ofNoFill();
+						ofTriangle( x * size, y * size, ( x + 1 ) * size, y * size, x * size, ( y + 1 ) * size );
+					}
 				}
 				
-				nx = field->noiseX +  0.75 * size * field->noiseXRes;
-				ny = field->noiseY +  0.75 * size * field->noiseYRes;
+				nx = field->noiseX +  0.75 * size * field->noiseXRes * field->noiseXScl;
+				ny = field->noiseY +  0.75 * size * field->noiseYRes * field->noiseYScl;
 				n  = ( noise.noise( nx, ny, field->noiseZ ) + 1.0 ) * 0.5;
 				
-				if( n < field->cutoff )
+				if( n > field->cutoff )
 				{
-					n  /= field->cutoff;
+					n  = ( n - field->cutoff ) / (float)( 1 - field->cutoff );
+					
 					cr = (int)( n * ( field->eColor[ 0 ] - field->sColor[ 0 ] ) + field->sColor[ 0 ] );
 					cg = (int)( n * ( field->eColor[ 1 ] - field->sColor[ 1 ] ) + field->sColor[ 1 ] );
 					cb = (int)( n * ( field->eColor[ 2 ] - field->sColor[ 2 ] ) + field->sColor[ 2 ] );
@@ -352,8 +435,18 @@ void testApp :: drawTriangleNoise ( TriangleField *field )
 					ofSetColor( cr, cg, cb, ca );
 					ofFill();
 					ofTriangle( x * size, ( y + 1 ) * size, ( x + 1 ) * size, ( y + 1 ) * size, ( x + 1 ) * size, y * size );
-					ofNoFill();
-					ofTriangle( x * size, ( y + 1 ) * size, ( x + 1 ) * size, ( y + 1 ) * size, ( x + 1 ) * size, y * size );
+					
+					if( fields->drawOutline )
+					{
+						cr = (int)( n * ( field->beColor[ 0 ] - field->bsColor[ 0 ] ) + field->bsColor[ 0 ] );
+						cg = (int)( n * ( field->beColor[ 1 ] - field->bsColor[ 1 ] ) + field->bsColor[ 1 ] );
+						cb = (int)( n * ( field->beColor[ 2 ] - field->bsColor[ 2 ] ) + field->bsColor[ 2 ] );
+						ca = (int)( n * ( field->beColor[ 3 ] - field->bsColor[ 3 ] ) + field->bsColor[ 3 ] );
+						
+						ofSetColor( cr, cg, cb, ca );
+						ofNoFill();
+						ofTriangle( x * size, ( y + 1 ) * size, ( x + 1 ) * size, ( y + 1 ) * size, ( x + 1 ) * size, y * size );
+					}
 				}
 			}
 		}
@@ -366,11 +459,15 @@ void testApp :: drawDebug()
 	ofDrawBitmapString
 	(
 		"fps        ::                     :: " + ofToString( ofGetFrameRate(), 2 ) + "\n\n" +
-		"fieldIndex :: press 1, 2 or 3     :: " + ofToString( fieldIndex, 0 ) + "\n\n" +
+		"\n\n" +
+		"fieldIndex :: press 1 - 9         :: " + ofToString( fieldIndex, 0 ) + "\n\n" +
 		"noiseRes   :: press q to change   :: " + ofToString( fields[ fieldIndex ].noiseXRes, 5 ) + "\n\n" +
 		"noiseXVel  :: press w to change   :: " + ofToString( fields[ fieldIndex ].noiseXVel, 5 ) + "\n\n" +
 		"noiseYVel  :: press e to change   :: " + ofToString( fields[ fieldIndex ].noiseYVel, 5 ) + "\n\n" +
 		"noiseZVel  :: press r to change   :: " + ofToString( fields[ fieldIndex ].noiseZVel, 5 ) + "\n\n" +
+		"scale      :: press t to change   :: " + ofToString( fields[ fieldIndex ].scale, 5 ) + "\n\n" +
+		"cutoff     :: press y to change   :: " + ofToString( fields[ fieldIndex ].cutoff, 5 ) + "\n\n" +
+		"\n\n" +
 		"dbInc      :: press -/+ to change :: " + ofToString( dbInc, 5 ) + "\n\n",
 		20,
 		20
@@ -389,14 +486,20 @@ void testApp :: keyPressed( int key )
 	if( key == '=' )
 		dbInc = 1;
 	
-	if( key == '1' )
+	if( key == '0' )
 		fieldIndex = 0;
-
-	if( key == '2' )
+	
+	if( key == '1' )
 		fieldIndex = 1;
 	
-	if( key == '3' )
+	if( key == '2' )
 		fieldIndex = 2;
+
+	if( key == '3' )
+		fieldIndex = 3;
+	
+	if( key == '4' )
+		fieldIndex = 4;
 	
 	if( key == 'q' )
 	{
@@ -413,6 +516,16 @@ void testApp :: keyPressed( int key )
 	
 	if( key == 'r' )
 		fields[ fieldIndex ].noiseZVel += 0.01 * dbInc;
+
+	if( key == 't' )
+	{
+		fields[ fieldIndex ].scaleInc = MIN( 10, MAX( 0, fields[ fieldIndex ].scaleInc + dbInc ) );
+		fields[ fieldIndex ].scale    = pow( 2, fields[ fieldIndex ].scaleInc );
+	}
+	
+	if( key == 'y' )
+		fields[ fieldIndex ].cutoff = MIN( 1, MAX( 0, fields[ fieldIndex ].cutoff += 0.01 * dbInc ) );
+
 }
 
 void testApp :: keyReleased( int key )
@@ -436,6 +549,9 @@ void testApp :: keyReleased( int key )
 			screenGrabUtil.start( "movie/trigfield" );
 		}
 	}
+	
+	if( key == 'd' )
+		showDebug = !showDebug;
 }
 
 void testApp :: mouseMoved( int x, int y )
