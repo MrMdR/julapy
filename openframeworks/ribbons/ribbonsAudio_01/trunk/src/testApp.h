826b4c4d7f75117b0a54391564c98675dbee71da
julapy <julapy@81ff1be0-9049-0410-8291-4d9ff92d881a>
2009-08-29 02:52:21 +0000

    heaps of changes with audio. this is the latest code using the ribbonLib.

--- openframeworks/ribbons/ribbonsAudio_01/trunk/src/testApp.h
+++ openframeworks/ribbons/ribbonsAudio_01/trunk/src/testApp.h
@@ -1,23 +1,31 @@
 #ifndef _TEST_APP
 #define _TEST_APP
 
-#define USE_VBO
 //#define USE_LIVE_AUDIO
 
 #include "ofMain.h"
+
+#include "Ribbon.h"
+#include "RibbonView.h"
+#include "RibbonFill.h"
+#include "RibbonFillView.h"
+#include "RibbonMesh.h"
+#include "RibbonMeshView.h"
+
+#include "AudioAbstract.h"
+#include "AudioFileSpectrum.h"
+#include "AudioLiveSpectrum.h"
+#include "RibbonAudio.h"
+
+#include "ofxColorMap.h"
 #include "SphereBg.h"
 #include "SphericalField.h"
 #include "NoiseField.h"
+
 #include "ofxTileSaver.h"
 #include "ofxScreenGrabUtil.h"
 
-#ifdef USE_LIVE_AUDIO
-	#include "RibbonLiveAudio.h"
-#else
-	#include "RibbonFileAudio.h"
-#endif
-
-#define MAX_PARTICLES		1000
+#define MAX_PARTICLES		2000
 #define MAX_TRAIL_LENGTH	100
 
 class testApp : public ofBaseApp{
@@ -28,12 +36,22 @@ class testApp : public ofBaseApp{
 		void update();
 		void draw();
 
-		void initVBO();
+		void initRenderArea();
+		void initApp();
+		void initColorMap();
+		void initShereBg();
+		void initFields();
+		void initAudio();
+
+		void initRibbons();
+		void updtRibbons();
+		void drawRibbons();
+		
 		void mapColour( float x, float y, int *r, int *g, int *b );
-		void drawPoint();
-		void drawRibbonFill();	
-		void drawRibbonFillVBO();
-		void drawRibbonMesh();
+
+		void drawDebug();
+	
+		void toggleFullScreen();
 	
 		void audioReceived(float * input, int bufferSize, int nChannels );
 		void keyPressed(int key);
@@ -44,45 +62,36 @@ class testApp : public ofBaseApp{
 		void mouseReleased(int x, int y, int button);
 		void windowResized(int w, int h);
 
-		int stageWidth;
-		int stageHeight;
-		int stageCenterX;
-		int stageCenterY;
-	
-		bool useAdditiveBlending;
-	
-		NoiseField		noiseField;
-		SphericalField	sphericalField;
-	
-		SphereBg		sphereBg;
-		ofImage			colourMapImage;
-	
-		int		pTotal;
-		int		posSpread;
-		int		velSpread;
-		int		trailIndex;
-		float	pos[ MAX_PARTICLES ][ 3 ];
-		float	vel[ MAX_PARTICLES ][ 3 ];
-		float	col[ MAX_PARTICLES ][ 4 ];
-		float	trl[ MAX_PARTICLES ][ 3 * MAX_TRAIL_LENGTH ];		// trail position.
-		float	tvd[ MAX_PARTICLES ][ 3 * MAX_TRAIL_LENGTH ];		// trail vertex direction.
-		float	tvr[ MAX_PARTICLES ][ 3 * MAX_TRAIL_LENGTH * 2 ];	// trail vertexes.
-		float	tcl[ MAX_PARTICLES ][ 4 * MAX_TRAIL_LENGTH * 2 ];	// trail colour.
-		GLuint	vbo[ MAX_PARTICLES * 2 ];
-	
-		ofxVec3f	upAxis;
-		float		upAxisRot;
-		float		rotateY;
+		ofRectangle		renderArea;
+		ofRectangle		renderAreaWindow;
+		ofRectangle		renderAreaFullScreen;
 	
 		ofxTileSaver		tileSaver;
 		ofxScreenGrabUtil	screenGrabUtil;
 	
-#ifdef USE_LIVE_AUDIO
-		RibbonLiveAudio		ribbonLiveAudio;
-#else
-		RibbonFileAudio		ribbonFileAudio;
-#endif
+		bool debugMode;
+		int  incDir;
 	
+		ofxColorMap		colorMap;
+		SphereBg		sphereBg;
+	
+		NoiseField		noiseField;
+		SphericalField	sphericalField;
+	
+		int		pTotal;
+		int		posSpread;
+		int		velSpread;
+
+		int		vboGlitch[ MAX_TRAIL_LENGTH ];
+	
+		Ribbon		**ribbons;
+		RibbonView	**ribbonViews;
+	
+		float		rotateY;
+	
+		AudioAbstract	*audio;
+		RibbonAudio		ribbonAudio;
+		
 };
 
 #endif
