60e5cbf2d02cec069de59184b868f0c1da8372da
julapy <julapy@81ff1be0-9049-0410-8291-4d9ff92d881a>
2009-10-17 22:29:44 +0000

    added ofxCvWarper

---
+++ trunk/openframeworks/ofxCvWarper/trunk/ofxCvWarper.cpp
@@ -0,0 +1,20 @@
+/*
+ *  ofxCvWarper.cpp
+ *  openFrameworks
+ *
+ *  Created by lukasz karluk on 17/10/09.
+ *  Copyright 2009 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "ofxCvWarper.h"
+
+ofxCvWarper :: ofxCvWarper()
+{
+	//
+}
+
+ofxCvWarper :: ~ofxCvWarper()
+{
+	//
+}
---
+++ trunk/openframeworks/ofxCvWarper/trunk/ofxCvWarper.h
@@ -0,0 +1,23 @@
+/*
+ *  ofxCvWarper.h
+ *  openFrameworks
+ *
+ *  Created by lukasz karluk on 17/10/09.
+ *  Copyright 2009 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#ifndef _OFX_CV_WARPER_
+#define _OFX_CV_WARPER_
+
+class ofxCvWarper
+{
+
+public :
+	
+	ofxCvWarper();
+	~ofxCvWarper();
+	
+};
+
+#endif
