498207c630f311d97dc85b616a08a54502c51b8c
julapy <julapy@81ff1be0-9049-0410-8291-4d9ff92d881a>
2009-12-23 23:59:31 +0000

    added ofxColorUtils

---
+++ trunk/openframeworks/ofxJulapyUtils/ofxColorUtils.h
@@ -0,0 +1,15 @@
+/*
+ *  ofxColorUtils.h
+ *  julapy_sydfest
+ *
+ *  Created by lukasz karluk on 23/12/09.
+ *  Copyright 2009 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#ifndef _OFX_COLOR_UTILS_H_
+#define _OFX_COLOR_UTILS_H_
+
+int ofRgbToHex ( int r, int g, int b );
+
+#endif
