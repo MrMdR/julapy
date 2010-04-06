63754c1192ed28771742061703c64a59f1962b66
julapy <julapy@81ff1be0-9049-0410-8291-4d9ff92d881a>
2010-04-06 02:28:15 +0000

    moved code from testApp into ColorTracker

--- trunk/openframeworks/colorTracker/trunk/src/testApp.h
+++ trunk/openframeworks/colorTracker/trunk/src/testApp.h
@@ -2,52 +2,7 @@
 #define _TEST_APP
 
 #include "ofMain.h"
-#include "ofxSimpleGuiToo.h"
-#include "ofxNetwork.h"
-#include "ofxOpenCv.h"
-#include "laserTracking.h"
-#include "ofContourAnalysis.h"
-#include "contourSimplify.h"
-
-#include <fstream>
-
-#define		MAX_NUM_CONTOURS_TO_FIND	20
-#define		TOTAL_OBJECTS_TRACKED		3
-
-struct HSVData
-{
-	float	hue, sat, val;
-	float	hueRange, satRange, valRange;
-	
-	int		minHue, maxHue;
-	int		minSat, maxSat;
-	int		minVal, maxVal;
-	
-	ofxCvGrayscaleImage		img;
-	unsigned char *         imgPixels;
-	
-	bool	bShowImg;
-	bool	bShowContour;
-};
-
-struct ContourData
-{
-	int						nBlobs;
-
-	vector<float>			blobArea;
-	vector<ofRectangle>		blobBoundingRect;
-	vector<ofPoint>			blobCentroid;
-	
-	CvBox2D32f				*box;
-	float					*blobAngle;
-	
-	vector<ofxPoint4f>      geomLines;
-	
-	vector<ofxPoint2f>		*contourReg;
-	vector<ofxPoint2f>		*contourSmooth;
-	vector<ofxPoint2f>		*contourSimple;
-	vector<ofxPoint2f>		*contourHull;
-};
+#include "ColorTracker.h"
 
 class testApp : public ofSimpleApp
 {
@@ -57,64 +12,14 @@ public:
 	void setup	();
 	void update	();
 	void draw	();
-	
-	void initContourAnalysis	();
-	void initTCP				();
-	void initGui				();
-	
-	void sendTrackerData		();
-	
-	void computeContourAnalysis ( int i );
-	void drawContourAnalysis	( int i );
-	
-	void writeToFile	( string filename = "config" );
-	void loadFromFile	( string filename = "config" );
 
 	void keyPressed		( int key );
 	void mouseMoved		( int x, int y );
 	void mouseDragged	( int x, int y, int button );
 	void mousePressed	( int x, int y, int button );
 	void mouseReleased	();
-
-	ofVideoGrabber			video;
-	ofRectangle				videoRect;
-
-	ofxCvColorImage		    colImg;
-	ofxCvColorImage		    hsvImg;
-	ofxCvGrayscaleImage		hueImg;
-	ofxCvGrayscaleImage		satImg;
-	ofxCvGrayscaleImage		valImg;
 	
-	ofxTCPServer			TCP;
-
-	float	blur;
-	float	threshold;
-	
-	HSVData			*hsv;
-	int				hsvTotal;
-	int				hsvIndex;
-	
-	ContourData		*cdata;
-	int				cdataTotal;
-	
-	ofxCvContourFinder		contourFinder;
-	contourSimplify			contourS;
-
-	float					smoothPct;
-	float					tolerance;
-	
-	int						cfDetail;
-	float					cfMinArea;
-	
-	bool					bShowRect;
-	bool					bShowBox;
-	bool					bShowEllipse;
-	bool					bShowAngle;
-	bool					bShowLines;
-	
-protected:
-
-	laserTracking LT;	
+	ColorTracker	ct;
 	
 };
 
