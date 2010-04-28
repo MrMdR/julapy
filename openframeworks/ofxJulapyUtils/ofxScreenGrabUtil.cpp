5e24ec4fe405f3640773a9cbfd997d95a1db09b1
julapy <julapy@81ff1be0-9049-0410-8291-4d9ff92d881a>
2010-04-28 01:21:04 +0000

    cleaned up ofxScreenGrabUtil with some newly acquired knowledge in c++ land.

--- openframeworks/ofxJulapyUtils/ofxScreenGrabUtil.cpp
+++ openframeworks/ofxJulapyUtils/ofxScreenGrabUtil.cpp
@@ -7,49 +7,66 @@
  *
  */
 
-#include "ofMain.h"
 #include "ofxScreenGrabUtil.h"
 
 ofxScreenGrabUtil :: ofxScreenGrabUtil()
 {
-	fileName		= "image";
-	recording		= false;
-	counter			= 0;
-	
-	saveArea.x		= 0;
-	saveArea.y		= 0;
-	saveArea.width	= ofGetWidth();
-	saveArea.height	= ofGetHeight();
+	//
 }
 
 ofxScreenGrabUtil :: ~ofxScreenGrabUtil()
 {
-	
+	//
 }
 
-void ofxScreenGrabUtil :: setup( string name, ofRectangle *rect )
+void ofxScreenGrabUtil :: setup()
 {
-	fileName = name;
-	
-	saveArea.x		= rect->x;
-	saveArea.y		= rect->y;
-	saveArea.width	= rect->width;
-	saveArea.height	= rect->height;
+	setup( "" );
 }
 
-void ofxScreenGrabUtil :: start( )
+void ofxScreenGrabUtil :: setup( string path )
 {
-	recording	= true;
-	counter		= 0;
+	ofRectangle	rect;
+	rect.x		= 0;
+	rect.y		= 0;
+	rect.width	= ofGetWidth();
+	rect.height	= ofGetHeight();
+	
+	setup( path, rect );
+}
+
+void ofxScreenGrabUtil :: setup( string path, const ofRectangle& rect )
+{
+	fileName		= ofToDataPath( path );
+	
+	saveArea.x		= rect.x;
+	saveArea.y		= rect.y;
+	saveArea.width	= rect.width;
+	saveArea.height	= rect.height;
+	
+	reset();
+}
+
+void ofxScreenGrabUtil :: setPause( bool value )
+{
+	recording = !value;
+}
+
+void ofxScreenGrabUtil :: togglePause()
+{
+	recording = !recording;
 }
 
 void ofxScreenGrabUtil :: save()
 {
+	if( !recording )
+		return;
+	
 	image.grabScreen( saveArea.x, saveArea.y, saveArea.width, saveArea.height );
 	
 	string imageFileNameStr = fileName;
 	char imageFileName[ 255 ];
-	sprintf( imageFileName, "_%02d%02d%02d_%05d.png", ofGetYear() % 1000, ofGetMonth(), ofGetDay(), counter );
+	sprintf( imageFileName, "frame_%02d%02d%02d_%05d.png", ofGetYear() % 1000, ofGetMonth(), ofGetDay(), counter );
 	imageFileNameStr.insert( imageFileNameStr.size(), imageFileName );
 
 	image.saveImage( imageFileNameStr );
@@ -57,20 +74,13 @@ void ofxScreenGrabUtil :: save()
 	counter++;
 }
 
-void ofxScreenGrabUtil :: stop()
+void ofxScreenGrabUtil :: reset ()
 {
-	recording = false;
+	recording	= false;
+	counter		= 0;
 }
 
 bool ofxScreenGrabUtil :: isRecording()
 {
 	return recording;
-}
-
-void ofxScreenGrabUtil :: setArea( ofRectangle *rect )
-{
-	saveArea.x		= rect->x;
-	saveArea.y		= rect->y;
-	saveArea.width	= rect->width;
-	saveArea.height	= rect->height;
 }
