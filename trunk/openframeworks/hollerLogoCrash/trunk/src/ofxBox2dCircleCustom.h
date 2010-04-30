cc7fdde2c7c6315edca7dc0a7099f5089bd5fb61
julapy <julapy@81ff1be0-9049-0410-8291-4d9ff92d881a>
2010-04-30 07:53:23 +0000

    just put in a bit of bounce.

--- trunk/openframeworks/hollerLogoCrash/trunk/src/ofxBox2dCircleCustom.h
+++ trunk/openframeworks/hollerLogoCrash/trunk/src/ofxBox2dCircleCustom.h
@@ -16,6 +16,64 @@ public :
 	
 	int color;
 	
+	ofImage* circleLayer1;
+	ofImage* circleLayer2;
+	ofImage* circleLayer3;
+	ofImage* circleLayer4;
+	
+	virtual void setImages( ofImage* layer1, ofImage* layer2, ofImage* layer3, ofImage* layer4 )
+	{
+		circleLayer1 = layer1;
+		circleLayer2 = layer2;
+		circleLayer3 = layer3;
+		circleLayer4 = layer4;
+	}
+	
+	virtual void drawImage ()
+	{
+		float scl;
+		scl = getRadius() / (float)circleLayer1->width;
+		
+		glPushMatrix();
+		glTranslatef
+		(
+			getPosition().x,
+			getPosition().y,
+			0
+		);
+		
+		float r;
+		r = getRotation();
+		
+		if( r >= 0 )
+			r += 180;
+		
+		glRotatef( r, 0, 0, 1 );
+		glScalef( scl * 2, scl * 2, 0 );
+		
+		ofEnableSmoothing();
+		ofEnableAlphaBlending();
+
+		ofSetColor( color );
+		
+		float cx, cy;
+		cx = -circleLayer1->width  * 0.5;
+		cy = -circleLayer1->height * 0.5;
+		
+		circleLayer1->draw( cx, cy );
+		
+		ofSetColor( 0xFFFFFF );
+		
+		circleLayer2->draw( cx, cy );
+		circleLayer3->draw( cx, cy );
+		circleLayer4->draw( cx, cy );
+
+		ofDisableSmoothing();
+		ofDisableAlphaBlending();
+		
+		glPopMatrix();
+	}
+	
 	virtual void draw() {
 		if(dead && !body) return;
 		
@@ -42,5 +100,7 @@ public :
 		
 		glPopMatrix();
 	}
+	
+	
 	
 };
