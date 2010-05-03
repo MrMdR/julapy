b84b7afb7b2d19cc66a53e85fd99a7bae8c41d97
julapy <julapy@81ff1be0-9049-0410-8291-4d9ff92d881a>
2010-05-03 06:35:30 +0000

    with more knowledge comes more changes. updated code and cleaned up.

--- openframeworks/faceTracker/ofxCvHaarTracker/trunk/src/ofxCvHaarTrackerItem.cpp
+++ openframeworks/faceTracker/ofxCvHaarTracker/trunk/src/ofxCvHaarTrackerItem.cpp
@@ -42,48 +42,52 @@ int ofxCvHaarTrackerItem :: getID ( )
 	return itemID;
 }
 
-void ofxCvHaarTrackerItem :: set( float x, float y, float w, float h )
+void ofxCvHaarTrackerItem :: set( const ofRectangle& haarRect )
 {
 	int i;
 	
 	for( i=0; i<rectBufferLimit; i++ )
 	{
 		rectBuffer.push_back( ofRectangle() );
-		rectBuffer.back().x			= x;
-		rectBuffer.back().y			= y;
-		rectBuffer.back().width		= w;
-		rectBuffer.back().height	= h;
+		rectBuffer.back().x			= haarRect.x;
+		rectBuffer.back().y			= haarRect.y;
+		rectBuffer.back().width		= haarRect.width;
+		rectBuffer.back().height	= haarRect.height;
 	}
 	
-	rect.x		= x;
-	rect.y		= y;
-	rect.width	= w;
-	rect.height = h;
+	rect.x			= haarRect.x;
+	rect.y			= haarRect.y;
+	rect.width		= haarRect.width;
+	rect.height		= haarRect.height;
 
-	rectEase.x		= x;
-	rectEase.y		= y;
-	rectEase.width	= w;
-	rectEase.height = h;
+	rectEase.x		= haarRect.x;
+	rectEase.y		= haarRect.y;
+	rectEase.width	= haarRect.width;
+	rectEase.height = haarRect.height;
 }
 
-void ofxCvHaarTrackerItem :: add( float x, float y, float w, float h )
+void ofxCvHaarTrackerItem :: add( const ofRectangle& haarRect )
 {
 	rectBuffer.erase( rectBuffer.begin() );
 	rectBuffer.push_back( ofRectangle() );
-	rectBuffer.back().x			= x;
-	rectBuffer.back().y			= y;
-	rectBuffer.back().width		= w;
-	rectBuffer.back().height	= h;
+	rectBuffer.back().x			= haarRect.x;
+	rectBuffer.back().y			= haarRect.y;
+	rectBuffer.back().width		= haarRect.width;
+	rectBuffer.back().height	= haarRect.height;
 	
-	rect.x		= x;
-	rect.y		= y;
-	rect.width	= w;
-	rect.height = h;
+	rect.x		= haarRect.x;
+	rect.y		= haarRect.y;
+	rect.width	= haarRect.width;
+	rect.height = haarRect.height;
 }
 
-bool ofxCvHaarTrackerItem :: checkItem( float x, float y, float w, float h )
+bool ofxCvHaarTrackerItem :: checkItem( const ofRectangle& haarRect )
 {
-	isMatch = checkAgainstCentreDistance( x + w / 2.0, y + h / 2.0 );
+	isMatch = checkAgainstCentreDistance
+	(
+		haarRect.x + haarRect.width  * 0.5,
+		haarRect.y + haarRect.height * 0.5
+	);
 	
 	return isMatch;
 }
@@ -93,8 +97,8 @@ bool ofxCvHaarTrackerItem :: checkAgainstCentreDistance ( float cx, float cy )
 	float dx, dy;
 	float len;
 	
-	dx = ( rect.x + rect.width / 2.0 ) - cx;
-	dy = ( rect.y + rect.height / 2.0 ) - cy;
+	dx = ( rect.x + rect.width  * 0.5 ) - cx;
+	dy = ( rect.y + rect.height * 0.5 ) - cy;
 
 	len = sqrt( dx * dx + dy * dy );
 	
@@ -136,7 +140,7 @@ void ofxCvHaarTrackerItem :: easeItemPosition ( float easeScale )
 
 void ofxCvHaarTrackerItem :: easeItemSize ( float easeScale )
 {
-	rectEase.width += ( rect.width - rectEase.width ) * easeScale;
+	rectEase.width  += ( rect.width  - rectEase.width )  * easeScale;
 	rectEase.height += ( rect.height - rectEase.height ) * easeScale;
 }
 
