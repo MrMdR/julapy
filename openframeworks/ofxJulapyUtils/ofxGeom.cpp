40294f29c9d3be8e0395da6d6eea8834798b13a5
julapy <julapy@81ff1be0-9049-0410-8291-4d9ff92d881a>
2010-08-04 08:20:49 +0000

    adding ofxGeom

---
+++ openframeworks/ofxJulapyUtils/ofxGeom.cpp
@@ -0,0 +1,20 @@
+/*
+ *  ofxGeom.cpp
+ *  emptyExample
+ *
+ *  Created by Lukasz Karluk on 2/08/10.
+ *  Copyright 2010 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "ofxGeom.h"
+
+ofxGeom :: ofxGeom ()
+{
+	//
+}
+
+ofxGeom :: ~ofxGeom ()
+{
+	//
+}
