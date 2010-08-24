e592a53f27f8d65e293929881e3ff93f4b3ed8b3
julapy <julapy@81ff1be0-9049-0410-8291-4d9ff92d881a>
2010-08-24 09:14:48 +0000

    initial import.

---
+++ openframeworks/colorCycle_ipad/src/testApp.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxiPhone.h"
+#include "ofxiPhoneExtras.h"
+
+#include "ColorCycle.h"
+#include "ofxColorPicker.h"
+#include "FooterBar.h"
+
+class testApp : public ofxiPhoneApp {
+	
+public:
+	void setup();
+	void update();
+	void draw();
+	void exit();
+	
+	void touchDown(ofTouchEventArgs &touch);
+	void touchMoved(ofTouchEventArgs &touch);
+	void touchUp(ofTouchEventArgs &touch);
+	void touchDoubleTap(ofTouchEventArgs &touch);
+
+	void lostFocus();
+	void gotFocus();
+	void gotMemoryWarning();
+	void deviceOrientationChanged(int newOrientation);
+
+	int				frameRate;
+	
+	ColorCycle		cc;
+	FooterBar*		footer;
+	
+};
+
+
---
+++ openframeworks/colorCycle_ipad/src/testApp.mm
@@ -0,0 +1,104 @@
+#include "testApp.h"
+
+//--------------------------------------------------------------
+void testApp::setup()
+{	
+	ofRegisterTouchEvents(this);
+	ofxAccelerometer.setup();
+	ofxiPhoneAlerts.addListener(this);
+	
+	iPhoneSetOrientation( OFXIPHONE_ORIENTATION_LANDSCAPE_LEFT );
+	
+	ofSetFrameRate( frameRate = 60 );
+//	ofSetVerticalSync( true );
+	
+	cc.setScreenSize( ofGetWidth(), ofGetHeight() );
+	cc.setup();
+	
+	footer = NULL;
+	footer = new FooterBar();
+	if( footer != NULL )
+		footer->setup();
+}
+
+//--------------------------------------------------------------
+void testApp::update()
+{
+	float gx;
+	gx = ofxAccelerometer.getForce().y;
+	gx *= 2;									// increase the reaction to tilt.
+	gx = MIN( 1.0, MAX( -1.0, gx ) );			// between -1 and 1.
+	gx *= ( ofxiPhoneGetOrientation() == OFXIPHONE_ORIENTATION_LANDSCAPE_RIGHT ) ? -1 : 1;
+	
+	float gy;
+	gy = ofxAccelerometer.getForce().x;
+	gy *= 2;									// increase the reaction to tilt.
+	gy = MIN( 1.0, MAX( -1.0, gy ) );			// between -1 and 1.
+	gy *= ( ofxiPhoneGetOrientation() == OFXIPHONE_ORIENTATION_LANDSCAPE_RIGHT ) ? -1 : 1;
+	
+	cc.setGravity( gx, gy );
+	
+	cc.update();
+}
+
+//--------------------------------------------------------------
+void testApp::draw()
+{
+	cc.draw();
+	
+	ofSetColor( 0, 0, 0 );
+	ofDrawBitmapString( ofToString( ofGetFrameRate(),  0 ), ofGetScreenWidth() - 30, 20 );
+}
+
+//--------------------------------------------------------------
+void testApp::exit(){
+
+}
+
+//--------------------------------------------------------------
+void testApp::touchDown(ofTouchEventArgs &touch)
+{
+	cc.down( touch.x, touch.y, touch.id );
+}
+
+//--------------------------------------------------------------
+void testApp::touchMoved(ofTouchEventArgs &touch)
+{
+	cc.drag( touch.x, touch.y, touch.id );
+}
+
+//--------------------------------------------------------------
+void testApp::touchUp(ofTouchEventArgs &touch)
+{
+	cc.up( touch.x, touch.y, touch.id );
+}
+
+//--------------------------------------------------------------
+void testApp::touchDoubleTap(ofTouchEventArgs &touch)
+{
+//	cc.resetJoints();
+	
+	if( footer != NULL )
+		footer->toggleShow();
+}
+
+//--------------------------------------------------------------
+void testApp::lostFocus(){
+
+}
+
+//--------------------------------------------------------------
+void testApp::gotFocus(){
+
+}
+
+//--------------------------------------------------------------
+void testApp::gotMemoryWarning(){
+
+}
+
+//--------------------------------------------------------------
+void testApp::deviceOrientationChanged(int newOrientation){
+
+}
+
