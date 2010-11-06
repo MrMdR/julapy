06fd44cf98798045061e373fb804634c04a4e078
julapy <julapy@81ff1be0-9049-0410-8291-4d9ff92d881a>
2010-11-06 12:55:10 +0000

    added MovieClip code.

--- openframeworks/ofxFlash/trunk/ofxFlashMovieClip.cpp
+++ openframeworks/ofxFlash/trunk/ofxFlashMovieClip.cpp
@@ -9,12 +9,147 @@
 
 #include "ofxFlashMovieClip.h"
 
-ofxFlashMovieClip :: ofxFlashMovieClip()
+ofxFlashMovieClip :: ofxFlashMovieClip( )
 {
 	typeID = OFX_FLASH_MOVIE_CLIP_TYPE;
+	
+	frameIndex = 0;
+	
+	play();
 }
 
 ofxFlashMovieClip :: ~ofxFlashMovieClip()
 {
+	//
+}
 
+///////////////////////////////////////////////
+//	CORE.
+///////////////////////////////////////////////
+
+void ofxFlashMovieClip :: setup ( int total )
+{
+	for( int i=0; i<total; i++ )
+	{
+		ofxFlashDisplayObjectContainer* frame;
+		frame = new ofxFlashDisplayObjectContainer();
+		frames.push_back( frame );
+	}
+	
+	frameIndex	= 0;
+	this->frame	= frames[ frameIndex ];
+}
+
+void ofxFlashMovieClip :: update ()
+{
+	if( totalFrames() < 2 )			// if only one frame, leave it, nothing to update.
+		return;
+	
+	if( bPlay )
+	{
+		removeFrameChildren();
+		nextFrame();
+		addFrameChildren();
+	}
+}
+
+void ofxFlashMovieClip :: draw ()
+{
+	//
+}
+
+///////////////////////////////////////////////
+//	FRAME CHILDREN.
+///////////////////////////////////////////////
+
+void ofxFlashMovieClip :: addFrameChildren ()
+{
+	for( int i=0; i<frame->children.size(); i++ )
+	{
+		addChild( frame->children[ i ] );
+	}
+}
+
+void ofxFlashMovieClip :: removeFrameChildren ()
+{
+	for( int i=0; i<frame->children.size(); i++ )
+	{
+		removeChild( frame->children[ i ] );
+	}
+}
+
+///////////////////////////////////////////////
+//	MOVIE CLIP METHODS.
+///////////////////////////////////////////////
+
+void ofxFlashMovieClip :: gotoAndPlay ( int frameNum )
+{
+	frameIndex	= frameNum - 1;
+	frameIndex	= ofClamp( frameIndex, 0, totalFrames() - 1 );
+	frame		= frames[ frameIndex ];
+	
+	play();
+}
+
+void ofxFlashMovieClip :: gotoAndStop ( int frameNum )
+{
+	frameIndex	= frameNum - 1;
+	frameIndex	= ofClamp( frameIndex, 0, totalFrames() - 1 );
+	frame		= frames[ frameIndex ];
+	
+	stop();
+}
+
+void ofxFlashMovieClip :: nextFrame ()
+{
+	if( ++frameIndex > totalFrames() - 1 )
+	{
+		frameIndex = 0;
+	}
+	
+	frame = frames[ frameIndex ];
+}
+
+void ofxFlashMovieClip :: prevFrame ()
+{
+	if( --frameIndex < 0 )
+	{
+		frameIndex = totalFrames() - 1;
+	}
+	
+	frame = frames[ frameIndex ];
+}
+
+void ofxFlashMovieClip :: play ()
+{
+	bPlay = true;
+}
+
+void ofxFlashMovieClip :: stop ()
+{
+	bPlay = false;
+}
+
+///////////////////////////////////////////////
+//	GETTERS.
+///////////////////////////////////////////////
+
+int ofxFlashMovieClip :: totalFrames ()
+{
+	return frames.size();
+}
+
+int ofxFlashMovieClip :: currentFrame ()
+{
+	return frameIndex + 1;
+}
+
+///////////////////////////////////////////////
+//	GETTERS FOR FRAME CONTAINER.
+///////////////////////////////////////////////
+
+ofxFlashDisplayObjectContainer* ofxFlashMovieClip :: getFrameContainer( int i )
+{
+	i = (int)ofClamp( i, 0, frames.size() - 1 );
+	return frames[ i ];
 }
