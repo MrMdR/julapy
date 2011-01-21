d584096000a351646a1ef663b6fb6b2c13e6846d
julapy <julapy@81ff1be0-9049-0410-8291-4d9ff92d881a>
2011-01-21 04:46:31 +0000

    unresolved error... recommitting from scratch

--- trunk/openframeworks/hollerClock_iPhone/src/main.mm
+++
@@ -1,9 +0,0 @@
-#include "ofMain.h"
-#include "clockApp.h"
-
-int main()
-{
-	ofSetupOpenGL( 1024, 768, OF_FULLSCREEN );			// <-------- setup the GL context
-//	ofSetupOpenGL( 480,  320, OF_FULLSCREEN );			// <-------- setup the GL context
-	ofRunApp( new clockApp() );
-}
