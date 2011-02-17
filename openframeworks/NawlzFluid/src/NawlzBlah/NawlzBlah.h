86837b2333734c19c7aa5609dc7ea1600659eac0
julapy <julapy@81ff1be0-9049-0410-8291-4d9ff92d881a>
2011-02-17 15:00:20 +0000

    got the particles in the first 3 episodes looking nice. now need to add fluid interaction.

---
+++ openframeworks/NawlzFluid/src/NawlzBlah/NawlzBlah.h
@@ -0,0 +1,57 @@
+/*
+ *  NawlzBlah.h
+ *  emptyExample
+ *
+ *  Created by lukasz karluk on 17/02/11.
+ *  Copyright 2011 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#pragma once
+
+#include "ofBaseApp.h"
+#include "ofTexture.h"
+
+#include "MSACore.h"
+#include "MSAFluid.h"
+
+#include "NawlzBlahParticle.h"
+
+using namespace MSA;
+
+class NawlzBlah : public ofBaseApp
+{
+	
+public:
+	
+	 NawlzBlah();
+	~NawlzBlah();
+	
+	void setup	();
+	void update	();
+	void draw	();
+	
+	void createBackgroundTexture	( unsigned char* pixels, int width, int height, int glType, int x=0, int y=0 );
+	void createParticleTexture		( unsigned char* pixels, int width, int height, int glType, int x=0, int y=0 );
+	
+	void addParticle	();
+	
+	void keyPressed		( int key );
+	void keyReleased	( int key );
+	void mouseMoved		( int x, int y );
+	void mouseDragged	( int x, int y, int button );
+	void mousePressed	( int x, int y, int button );
+	void mouseReleased	( int x, int y, int button );
+	void windowResized	( int w, int h );
+	
+	ofTexture*		backgroundTexture;
+	ofTexture*		particleTexture;
+	
+	vector<NawlzBlahParticle*> particles;
+	
+	float wanderRadius;
+	float wanderDistance;
+	float wanderChange;
+	float wanderEase;
+	
+};
