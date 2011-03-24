abcd470558a764f9de16471417590086e4576604
julapy <julapy@81ff1be0-9049-0410-8291-4d9ff92d881a>
2011-03-24 09:11:16 +0000

    added Infordel and Questions particles.

--- trunk/openframeworks/NawlzFluid/src/main.cpp
+++ trunk/openframeworks/NawlzFluid/src/main.cpp
@@ -5,6 +5,9 @@
 #include "NawlzFishBowlApp.h"
 #include "NawlzBlahApp.h"
 #include "NawlzSandApp.h"
+#include "NawlzJackInBoxApp.h"
+#include "NawlzInfodelApp.h"
+#include "NawlzQuestionsApp.h"
 
 //========================================================================
 int main( ){
@@ -12,8 +15,14 @@ int main( ){
     ofAppGlutWindow window;
 	ofSetupOpenGL( &window, 1024, 768, OF_WINDOW );			// <-------- setup the GL context
 
-//	ofRunApp( new NawlzFluidApp() );
-	ofRunApp( new NawlzFishBowlApp() );
-//	ofRunApp( new NawlzBlahApp() );
-//	ofRunApp( new NawlzSandApp() );
+    switch( 5 )
+    {
+        case 0 : ofRunApp( new NawlzFluidApp()      );  break;
+        case 1 : ofRunApp( new NawlzFishBowlApp()   );  break;
+        case 2 : ofRunApp( new NawlzBlahApp()       );  break;
+        case 3 : ofRunApp( new NawlzSandApp()       );  break;
+        case 4 : ofRunApp( new NawlzJackInBoxApp()  );  break;
+        case 5 : ofRunApp( new NawlzInfodelApp()    );  break;
+        case 6 : ofRunApp( new NawlzQuestionsApp()  );  break;
+    }
 }
