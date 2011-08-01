29221efe61926feae15b93d493f159f8529fd3da
julapy <julapy@81ff1be0-9049-0410-8291-4d9ff92d881a>
2011-08-01 07:09:49 +0000

    removed ofxResizeUtils.cpp and moved everything into the header file.

--- trunk/openframeworks/ofxJulapyUtils/ofxResizeUtil.h
+++ trunk/openframeworks/ofxJulapyUtils/ofxResizeUtil.h
@@ -17,9 +17,41 @@ class ofxResizeUtil
 
 public :
 	
-	static ofRectangle cropToSize ( const ofRectangle& srcRect, const ofRectangle& dstRect );
-	static ofRectangle fitToSize  ( const ofRectangle& srcRect, const ofRectangle& dstRect );
-
+	static ofRectangle cropToSize ( const ofRectangle& srcRect, const ofRectangle& dstRect )
+    {
+        float wRatio, hRatio, scale;
+        
+        wRatio = dstRect.width  / (float)srcRect.width;
+        hRatio = dstRect.height / (float)srcRect.height;
+        
+        scale = MAX( wRatio, hRatio );
+        
+        ofRectangle			rect;
+        rect.x		= ( dstRect.width  - ( srcRect.width  * scale ) ) * 0.5;
+        rect.y		= ( dstRect.height - ( srcRect.height * scale ) ) * 0.5;
+        rect.width	= srcRect.width  * scale;
+        rect.height	= srcRect.height * scale;
+        
+        return rect;
+    }
+    
+	static ofRectangle fitToSize  ( const ofRectangle& srcRect, const ofRectangle& dstRect )
+    {
+        float wRatio, hRatio, scale;
+        
+        wRatio = dstRect.width  / (float)srcRect.width;
+        hRatio = dstRect.height / (float)srcRect.height;
+        
+        scale = MIN( wRatio, hRatio );
+        
+        ofRectangle			rect;
+        rect.x		= ( dstRect.width  - ( srcRect.width  * scale ) ) * 0.5;
+        rect.y		= ( dstRect.height - ( srcRect.height * scale ) ) * 0.5;
+        rect.width	= srcRect.width  * scale;
+        rect.height	= srcRect.height * scale;
+        
+        return rect;
+    }
 };
 
 #endif
